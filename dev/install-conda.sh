043320ddecad133a4444007acc2a70a4cf27bb9f
Chen Haifeng <haifeng.chen@intel.com>
2022-01-12 07:07:28 +0800

    Fix the space issue in install-conda.sh

--- dev/install-conda.sh
+++ dev/install-conda.sh
@@ -4,6 +4,6 @@ wget \
         --quiet "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" \
         -O /tmp/miniconda.sh \
     && /bin/bash /tmp/miniconda.sh -b -u -p $HOME/anaconda3 \
-    && $HOME/anaconda3/bin/conda init \ 
+    && $HOME/anaconda3/bin/conda init \
     && rm /tmp/miniconda.sh \
     

c002c5ae1030de037b832b215e0a0d5bfae27c31
haojinIntel <hao.jin@intel.com>
2022-01-19 18:27:46 +0800

    Spark configuration scripts fixed (#42)
    * Fix the bug about circular reference problem
    
    * Correct the path for providers
    
    * Use dynamic allocating resource for spark

--- runtime/spark/scripts/update_config.sh
+++ runtime/spark/scripts/update_config.sh
@@ -20,15 +20,15 @@ cp -r $source_dir/* $output_dir
 master_hostname=$1
 if [ ! -n "${master_hostname}" ]; then
 	master_hostname=$(hostname) 
-fi   
+else
+        master_hostname=ip-${master_hostname//./-}
+fi
 
 total_memory=$(awk '($1 == "MemTotal:"){print $2/1024}' /proc/meminfo)
 total_vcores=$(cat /proc/cpuinfo | grep processor | wc -l)
 
-spark_executor_cores=2
-spark_executor_instances=2
-spark_executor_memory=1g
-spark_yarn_executor_memoryOverhead=384
+spark_executor_cores=1
+spark_executor_memory=2g
 
 cd $output_dir
 sed -i "s/master_hostname/${master_hostname}/g" `grep "master_hostname" -rl ./`
@@ -39,9 +39,16 @@ sed -i "s/{%yarn.nodemanager.resource.cpu-vcores%}/${total_vcores}/g" `grep "{%y
 sed -i "s/{%yarn.scheduler.maximum-allocation-vcores%}/${total_vcores}/g" `grep "{%yarn.scheduler.maximum-allocation-vcores%}" -rl ./`
 
 sed -i "s/{%spark.executor.cores%}/${spark_executor_cores}/g" `grep "{%spark.executor.cores%}" -rl ./`
-sed -i "s/{%spark.executor.instances%}/${spark_executor_instances}/g" `grep "{%spark.executor.instances%}" -rl ./`
 sed -i "s/{%spark.executor.memory%}/${spark_executor_memory}/g" `grep "{%spark.executor.memory%}" -rl ./`
-sed -i "s/{%spark.yarn.executor.memoryOverhead%}/${spark_yarn_executor_memoryOverhead}/g" `grep "{%spark.yarn.executor.memoryOverhead%}" -rl ./`
 
 cp -r ${output_dir}/hadoop/*  ${HADOOP_HOME}/etc/hadoop/
-cp -r ${output_dir}/spark/*  ${SPARK_HOME}/conf/spark/
+cp -r ${output_dir}/spark/*  ${SPARK_HOME}/conf
+
+jars=('spark-[0-9]*[0-9]-yarn-shuffle.jar' 'spark-network-common_[0-9]*[0-9].jar' 'spark-network-shuffle_[0-9]*[0-9].jar' 'jackson-databind-[0-9]*[0-9].jar' 'jackson-core-[0-9]*[0-9].jar' 'jackson-annotations-[0-9]*[0-9].jar' 'metrics-core-[0-9]*[0-9].jar' 'netty-all-[0-9]*[0-9].Final.jar' 'commons-lang3-[0-9]*[0-9].jar')
+find ${HADOOP_HOME}/share/hadoop/yarn/lib -name netty-all-[0-9]*[0-9].Final.jar| xargs -i mv -f {} {}.old
+for jar in ${jars[@]};
+do
+	find ${SPARK_HOME}/jars/ -name $jar | xargs -i cp {} ${HADOOP_HOME}/share/hadoop/yarn/lib; 
+	find ${SPARK_HOME}/yarn/ -name $jar | xargs -i cp {} ${HADOOP_HOME}/share/hadoop/yarn/lib;
+done
+

ff28281fe2a8c9f2cf67c6587a5819e68ade0bae
Chen Haifeng <haifeng.chen@intel.com>
2022-03-11 07:13:54 +0800

    Add spark examples to run spark pi, spark shell and spark-sql

---
+++ example/spark/spark-sql.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function show_usage() {
+  echo "Usage: spark-sql.sh cluster-config-file [--help]"
+}
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -h|--help)
+    shift 1 # past argument
+    show_usage
+    exit 1
+    ;;
+    *)    # cluster config file
+    cluster_config_file=$1
+    shift 1 # past argument
+    ;;
+esac
+done
+
+if [ -z "$cluster_config_file" ]
+then
+      echo "Error: cluster config file is not specified."
+      show_usage
+      exit 1
+fi
+
+cloudtik exec $cluster_config_file  \
+  "spark-sql"

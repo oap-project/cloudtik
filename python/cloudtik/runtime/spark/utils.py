dbf6824f6895db44b505d51e4d2ebb5630de48c2
Chen Haifeng <haifeng.chen@intel.com>
2022-03-27 15:30:48 +0800

    Improve the spark executor memory allocation with more robust algorithm (#271)
    * Improve the spark executor memory allocation with more robust algorithm
    
    * Simplify code a little

--- python/cloudtik/runtime/spark/utils.py
+++ python/cloudtik/runtime/spark/utils.py
@@ -12,8 +12,40 @@ SPARK_RUNTIME_PROCESSES = [
 ]
 
 YARN_RESOURCE_MEMORY_RATIO = 0.8
-SPARK_EXECUTOR_MEMORY_RATIO = 0.8
-SPARK_DRIVER_MEMORY_RATIO = 0.25
+SPARK_EXECUTOR_MEMORY_RATIO = 1
+SPARK_DRIVER_MEMORY_RATIO = 0.1
+SPARK_APP_MASTER_MEMORY_RATIO = 0.02
+SPARK_DRIVER_MEMORY_MINIMUM = 1024
+SPARK_DRIVER_MEMORY_MAXIMUM = 8192
+SPARK_EXECUTOR_CORES_DEFAULT = 4
+SPARK_ADDITIONAL_OVERHEAD = 1024
+
+
+def round_memory_size_to_gb(memory_size: int) -> int:
+    gb = int(memory_size / 1024)
+    if gb < 1:
+        gb = 1
+    return gb * 1024
+
+
+def get_spark_driver_memory(cluster_resource: Dict[str, Any]) -> int:
+    spark_driver_memory = round_memory_size_to_gb(
+        int(cluster_resource["head_memory"] * SPARK_DRIVER_MEMORY_RATIO))
+    return max(min(spark_driver_memory,
+               SPARK_DRIVER_MEMORY_MAXIMUM), SPARK_DRIVER_MEMORY_MINIMUM)
+
+
+def get_spark_app_master_memory(worker_memory_for_spark: int) -> int:
+    spark_app_master_memory = round_memory_size_to_gb(
+        int(worker_memory_for_spark * SPARK_APP_MASTER_MEMORY_RATIO))
+    return max(min(spark_app_master_memory,
+               SPARK_DRIVER_MEMORY_MAXIMUM), SPARK_DRIVER_MEMORY_MINIMUM)
+
+
+def get_spark_overhead(worker_memory_for_spark: int) -> int:
+    # Calculate the spark overhead including one app master based on worker_memory_for_spark
+    spark_app_master = get_spark_app_master_memory(worker_memory_for_spark)
+    return spark_app_master + SPARK_ADDITIONAL_OVERHEAD
 
 
 def config_spark_runtime_resources(
@@ -22,20 +54,36 @@ def config_spark_runtime_resources(
 
     container_resource = {}
     container_resource["yarn_container_maximum_vcores"] = cluster_resource["worker_cpu"]
-    container_resource["yarn_container_maximum_memory"] = \
-        int(cluster_resource["worker_memory"] * YARN_RESOURCE_MEMORY_RATIO)
+
+    worker_memory_for_yarn = round_memory_size_to_gb(
+        int(cluster_resource["worker_memory"] * YARN_RESOURCE_MEMORY_RATIO))
+    container_resource["yarn_container_maximum_memory"] = worker_memory_for_yarn
 
     executor_resource = {}
-    if int(cluster_resource["worker_cpu"]) < 4:
-        executor_resource["spark_executor_cores"] = cluster_resource["worker_cpu"]
-        executor_resource["spark_executor_memory"] = int(cluster_resource["worker_memory"] *
-                                                         YARN_RESOURCE_MEMORY_RATIO * SPARK_EXECUTOR_MEMORY_RATIO)
-    else:
-        worker_per_core_memory_MB = cluster_resource["worker_memory"] /  cluster_resource["worker_cpu"]
-        executor_resource["spark_executor_cores"] = 4
-        executor_resource["spark_executor_memory"] = int(worker_per_core_memory_MB * 4 *
-                                                         YARN_RESOURCE_MEMORY_RATIO * SPARK_EXECUTOR_MEMORY_RATIO)
-    executor_resource["spark_driver_memory"] = min(int(cluster_resource["head_memory"] * SPARK_DRIVER_MEMORY_RATIO), 8192)
+    executor_resource["spark_driver_memory"] = get_spark_driver_memory(cluster_resource)
+
+    # Calculate Spark executor cores
+    spark_executor_cores = SPARK_EXECUTOR_CORES_DEFAULT
+    if spark_executor_cores > cluster_resource["worker_cpu"]:
+        spark_executor_cores = cluster_resource["worker_cpu"]
+
+    executor_resource["spark_executor_cores"] = spark_executor_cores
+
+    # For Spark executor memory, we use the following formula:
+    # x = worker_memory_for_yarn
+    # n = number_of_executors
+    # m = spark_executor_memory
+    # a = spark_overhead (app_master_memory + others)
+    # x = n * m + a
+
+    number_of_executors = int(cluster_resource["worker_cpu"] / spark_executor_cores)
+    worker_memory_for_spark = round_memory_size_to_gb(
+        int(worker_memory_for_yarn * SPARK_EXECUTOR_MEMORY_RATIO))
+    spark_overhead = round_memory_size_to_gb(
+        get_spark_overhead(worker_memory_for_spark))
+    worker_memory_for_executors = worker_memory_for_spark - spark_overhead
+    executor_resource["spark_executor_memory"] = round_memory_size_to_gb(
+        int(worker_memory_for_executors / number_of_executors))
 
     if "runtime" not in cluster_config:
         cluster_config["runtime"] = {}

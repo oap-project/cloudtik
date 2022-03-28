174c4b359915f847fa271a6ae05ef17669562dd5
Haifeng Chen <haifeng.chen@intel.com>
2022-03-28 17:08:01 +0800

    Improve the AWS workspace creating messages

--- python/cloudtik/providers/_private/aws/workspace_provider.py
+++ python/cloudtik/providers/_private/aws/workspace_provider.py
@@ -13,25 +13,20 @@ class AWSWorkspaceProvider(WorkspaceProvider):
     def __init__(self, provider_config, workspace_name):
         WorkspaceProvider.__init__(self, provider_config, workspace_name)
 
-
     def create_workspace(self, cluster_config):
         return create_aws_workspace(cluster_config)
-
 
     def delete_workspace(self, cluster_config):
         return delete_workspace_aws(cluster_config)
 
-
     def check_workspace_resource(self, cluster_config):
         return check_aws_workspace_resource(cluster_config)
-
 
     @staticmethod
     def validate_config(
             provider_config: Dict[str, Any]) -> None:
         """Required Dicts indicate that no extra fields can be introduced."""
         return None
-
 
     @staticmethod
     def bootstrap_workspace_config(cluster_config):

ec350f1f10718de81b7be06d51c18e6b1a21387f
Chen Haifeng <haifeng.chen@intel.com>
2022-04-04 19:41:48 +0800

    Improve tpcds datagen with spark driver conf (#92)

--- tools/spark/benchmark/scripts/tpcds_datagen.scala
+++ tools/spark/benchmark/scripts/tpcds_datagen.scala
@@ -1,17 +1,29 @@
-val scale = "1"                   // data scale 1GB
-val format = "parquet"            // support parquer or orc
-val partitionTables = true        // create partition table
-val storage = "s3a"                // support hdfs or s3
-var bucket_name = "cloudtik-bucket"   // when storage is "s3", this value will be use.
+val conf = spark.sparkContext.getConf
+
+// data scale in GB
+val scale = conf.getInt("spark.driver.scale", 1)
+// support parquet or orc
+val format = conf.get("spark.driver.format", "parquet")
+// create partitioned table
+val partitionTables = conf.getBoolean("spark.driver.partitioned", true)
+// support s3a://s3_bucket, gs://gs_bucket
+// wasbs://container@storage_account.blob.core.windows.net
+// abfs://container@storage_account.dfs.core.windows.net
+val fsdir = conf.get("spark.driver.fsdir", "")
 val useDoubleForDecimal = false   // use double format instead of decimal format
 
+if (fsdir == "") {
+    println("File system dir must be specified with --conf spark.driver.fsdir")
+    sys.exit(0)
+}
+
 val tools_path = "/home/cloudtik/runtime/benchmark-tools/tpcds-kit/tools"
-val data_path = s"${storage}://${bucket_name}/datagen/tpcds_${format}/${scale}"
+val data_path = s"${fsdir}/datagen/tpcds_${format}/${scale}"
 val database_name = s"tpcds_${format}_scale_${scale}_db"
 val codec = "snappy"
 val clusterByPartitionColumns = partitionTables
 
-val p = scale.toInt / 2048.0
+val p = scale / 2048.0
 val catalog_returns_p = (263 * p + 1).toInt
 val catalog_sales_p = (2285 * p * 0.5 * 0.5 + 1).toInt
 val store_returns_p = (429 * p + 1).toInt
@@ -19,10 +31,9 @@ val store_sales_p = (3164 * p * 0.5 * 0.5 + 1).toInt
 val web_returns_p = (198 * p + 1).toInt
 val web_sales_p = (1207 * p * 0.5 * 0.5 + 1).toInt
 
-
 import com.databricks.spark.sql.perf.tpcds.TPCDSTables
-val sc = spark.sqlContext
 
+val sc = spark.sqlContext
 sc.setConf(s"spark.sql.$format.compression.codec", codec)
 
 val tables = new TPCDSTables(spark.sqlContext, tools_path, scale, useDoubleForDecimal)
@@ -51,3 +62,5 @@ tables.genData(data_path, format, true, partitionTables, clusterByPartitionColum
 tables.genData(data_path, format, true, partitionTables, clusterByPartitionColumns, false, "web_sales", web_sales_p)
 tables.genData(data_path, format, true, partitionTables, clusterByPartitionColumns, false, "web_returns", web_returns_p)
 tables.createExternalTables(data_path, format, database_name, overwrite = true, discoverPartitions = partitionTables)
+
+sys.exit(0)

b8c913514491eb1c78d59d4f1749ff6138a01598
haojinIntel <hao.jin@intel.com>
2022-04-07 12:05:42 +0800

    Add the function to delete and create virtual network for Azure

--- python/cloudtik/providers/_private/_azure/config.py
+++ python/cloudtik/providers/_private/_azure/config.py
@@ -12,6 +12,7 @@ from cloudtik.core._private.cli_logger import cli_logger, cf
 from azure.common.credentials import get_cli_profile
 from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resource.resources.models import DeploymentMode
 
 
@@ -48,7 +49,17 @@ def get_resource_group_name(config, resource_client, use_internal_ips):
         resource_group_name = get_working_node_resource_group_name()
     else:
         resource_group_name = get_workspace_resource_group_name(config, resource_client)
+
     return resource_group_name
+
+
+def get_virtual_network_name(config, resource_client, network_client, use_internal_ips):
+    if use_internal_ips:
+        virtual_network_name = get_working_node_virtual_network_name(resource_client, network_client)
+    else:
+        virtual_network_name = get_workspace_virtual_network_name(config, network_client)
+
+    return virtual_network_name
 
 
 def delete_workspace_azure(config):
@@ -67,8 +78,17 @@ def delete_workspace_azure(config):
         total_steps += 1
 
     try:
+        # delete network resources
         with cli_logger.group("Deleting workspace: {}", workspace_name):
             _delete_network_resources(config, resource_client, current_step, total_steps)
+
+        # delete resource group
+        if not use_internal_ips:
+            with cli_logger.group(
+                    "Deleting Resource Group",
+                    _numbered=("[]", current_step, total_steps)):
+                current_step += 1
+                _delete_resource_group(config, resource_client)
 
     except Exception as e:
         cli_logger.error(
@@ -83,6 +103,7 @@ def delete_workspace_azure(config):
 
 def _delete_network_resources(config, resource_client, current_step, total_steps):
     use_internal_ips = config["provider"].get("use_internal_ips", False)
+    network_client = construct_network_client(config)
 
     """
          Do the work - order of operation
@@ -90,8 +111,7 @@ def _delete_network_resources(config, resource_client, current_step, total_steps
          2.) Delete router for private subnet 
          3.) Delete private subnets
          4.) Delete firewalls
-         5.) Delete vpc
-         6.) Delete resource group
+         5.) Delete virtual network
     """
 
     # delete public subnets
@@ -122,21 +142,37 @@ def _delete_network_resources(config, resource_client, current_step, total_steps
     #     current_step += 1
     #     _delete_firewalls(config, compute)
 
-    # delete vpc
-    # if not use_internal_ips:
-    #     with cli_logger.group(
-    #             "Deleting VPC",
-    #             _numbered=("[]", current_step, total_steps)):
-    #         current_step += 1
-    #         _delete_vpc(config, compute)
-
-    # delete resource group
+    # delete virtual network
     if not use_internal_ips:
         with cli_logger.group(
-                "Deleting Resource Group",
+                "Deleting VPC",
                 _numbered=("[]", current_step, total_steps)):
             current_step += 1
-            _delete_resource_group(config, resource_client)
+            _delete_vnet(config, resource_client, network_client)
+
+
+def _delete_vnet(config, resource_client, network_client):
+    use_internal_ips = config["provider"].get("use_internal_ips", False)
+    resource_group_name = get_resource_group_name(config, resource_client, use_internal_ips)
+    virtual_network_name = get_virtual_network_name(config, resource_client, network_client, use_internal_ips)
+    if virtual_network_name is None:
+        cli_logger.print("This Virtual Network: {} has not existed. No need to delete it.".
+                         format(virtual_network_name))
+        return
+
+    """ Delete the Virtual Network """
+    cli_logger.print("Deleting the Virtual Network: {}...".format(virtual_network_name))
+    try:
+        network_client.virtual_networks.begin_delete(
+            resource_group_name=resource_group_name,
+            virtual_network_name=virtual_network_name
+        ).result()
+        cli_logger.print("Successfully deleted the Virtual Network: {}.".format(virtual_network_name))
+    except Exception as e:
+        cli_logger.error("Failed to delete the Virtual Network:{}. {}".format(virtual_network_name, str(e)))
+        raise e
+
+    return
 
 
 def _delete_resource_group(config, resource_client):
@@ -176,9 +212,21 @@ def _configure_workspace(config):
 
     current_step = 1
     total_steps = NUM_AZURE_WORKSPACE_CREATION_STEPS
+
+    resource_client = construct_resource_client(config)
+
     try:
+        # create resource group
+        with cli_logger.group(
+                "Creating Resource Group",
+                _numbered=("[]", current_step, total_steps)):
+            current_step += 1
+            _create_resource_group(config, resource_client)
+
+        # create network resources
         with cli_logger.group("Creating workspace: {}", workspace_name):
             config = _configure_network_resources(config, current_step, total_steps)
+
     except Exception as e:
         cli_logger.error("Failed to create workspace. {}", str(e))
         raise e
@@ -228,15 +276,69 @@ def get_azure_instance_metadata():
             "Failed to get instance metadata: {}", str(e))
         return None
 
+
 def get_working_node_resource_group_name():
     metadata = get_azure_instance_metadata()
     if metadata is None:
-        cli_logger.error("Failed to get the ResourceGroupId of the working node. "
+        cli_logger.error("Failed to get the metadata of the working node. "
                          "Please check whether the working node is a Azure instance or not!")
     resource_group_name = metadata.get("compute", {}).get("name", "")
     cli_logger.print("Successfully get the ResourceGroupName for working node.")
 
     return resource_group_name
+
+
+def get_virtual_network_name_by_subnet(resource_client, network_client, resource_group_name, subnet):
+    subnet_address_prefix = subnet['address'] + "/" + subnet['prefix']
+    virtual_networks_resources = list(resource_client.resources.list_by_resource_group(
+        resource_group_name=resource_group_name, filter="resourceType eq 'Microsoft.Network/virtualNetworks'"))
+    virtual_network_names = [virtual_network_resources.name for virtual_network_resources in virtual_networks_resources]
+    virtual_networks = [network_client.virtual_networks.get(
+        resource_group_name=resource_group_name, virtual_network_name=virtual_network_name)
+        for virtual_network_name in virtual_network_names]
+
+    for virtual_network in virtual_networks:
+        for subnet in virtual_network.subnets:
+            print(subnet.address_prefix)
+            if subnet.address_prefix == subnet_address_prefix:
+                return virtual_network.name
+
+    return None
+
+
+def get_working_node_virtual_network_name(resource_client, network_client):
+    metadata = get_azure_instance_metadata()
+    if metadata is None:
+        cli_logger.error("Failed to get the metadata of the working node. "
+                         "Please check whether the working node is a Azure instance or not!")
+    resource_group_name = metadata.get("compute", {}).get("name", "")
+    interfaces = metadata.get("network", {}).get("interface", "")
+    subnet = interfaces[0]["ipv4"]["subnet"][0]
+    virtual_network_name = get_virtual_network_name_by_subnet(resource_client, network_client, resource_group_name, subnet)
+    if virtual_network_name is not None:
+        cli_logger.print("Successfully get the VirtualNetworkName for working node.")
+
+    return virtual_network_name
+
+
+def get_workspace_virtual_network_name(config, network_client):
+    resource_group_name = 'cloudtik-{}-resource-group'.format(config["workspace_name"])
+    virtual_network_name = 'cloudtik-{}-vnet'.format(config["workspace_name"])
+    cli_logger.verbose("Getting the VirtualNetworkName for workspace: {}...".
+                       format(virtual_network_name))
+
+    try:
+        virtual_network = network_client.virtual_networks.get(
+            resource_group_name=resource_group_name,
+            virtual_network_name=virtual_network_name
+        )
+        cli_logger.verbose_error("Successfully get the VirtualNetworkName: {} for workspace.".
+                                 format(virtual_network_name))
+        return virtual_network.name
+    except Exception as e:
+        cli_logger.verbose_error(
+            "The Virtual Network for workspace is not found: {}", str(e))
+        return None
 
 
 def get_workspace_resource_group_name(config, resource_client):
@@ -248,7 +350,7 @@ def get_workspace_resource_group_name(config, resource_client):
         resource_group = resource_client.resource_groups.get(
             resource_group_name
         )
-        cli_logger.verbose_error("Successfully get the ResourceGroupName of {} for workspace.".
+        cli_logger.verbose("Successfully get the ResourceGroupName: {} for workspace.".
                                  format(resource_group_name))
         return resource_group.name
     except Exception as e:
@@ -258,18 +360,18 @@ def get_workspace_resource_group_name(config, resource_client):
 
 
 def create_resource_group(config, resource_client):
-    resource_group = 'cloudtik-{}-resource-group'.format(config["workspace_name"])
+    resource_group_name = 'cloudtik-{}-resource-group'.format(config["workspace_name"])
 
     assert "location" in config["provider"], (
         "Provider config must include location field")
     params = {"location": config["provider"]["location"]}
-    cli_logger.print("Creating workspace Resource Group: {} on Azure...", resource_group)
+    cli_logger.print("Creating workspace Resource Group: {} on Azure...", resource_group_name)
     # create resource group
     try:
 
         resource_group = resource_client.resource_groups.create_or_update(
-            resource_group_name=resource_group, parameters=params)
-        time.sleep(20)
+            resource_group_name=resource_group_name, parameters=params)
+        # time.sleep(20)
         cli_logger.print("Successfully created workspace Resource Group: cloudtik-{}-resource_group.".
                          format(config["workspace_name"]))
         return resource_group
@@ -278,22 +380,97 @@ def create_resource_group(config, resource_client):
             "Failed to create workspace Resource Group. {}", str(e))
         raise e
 
+
+# def _create_resource_group(config, resource_client):
+#     workspace_name = config["workspace_name"]
+#     use_internal_ips = config["provider"].get("use_internal_ips", False)
+#
+#     if use_internal_ips:
+#         # No need to create new resource group
+#         resource_group_name = get_working_node_resource_group_name()
+#         if resource_group_name is None:
+#             cli_logger.abort("Only when the working node is "
+#                              "an Azure instance can use use_internal_ips=True.")
+#     else:
+#
+#         # Need to create a new resource_group
+#         if get_workspace_resource_group_name(config, resource_client) is None:
+#             resource_group = create_resource_group(config, resource_client)
+#             resource_group_name = resource_group.name
+#         else:
+#             cli_logger.abort("There is a existing Resource Group with the same name: {}, "
+#                              "if you want to create a new workspace with the same name, "
+#                              "you need to execute workspace delete first!".format(workspace_name))
+#     return resource_group_name
+
+
+def _create_vnet(config, resource_client, network_client):
+    workspace_name = config["workspace_name"]
+    use_internal_ips = config["provider"].get("use_internal_ips", False)
+
+    if use_internal_ips:
+        # No need to create new virtual network
+        virtual_network_name = get_working_node_virtual_network_name(resource_client, network_client)
+        if virtual_network_name is None:
+            cli_logger.abort("Only when the working node is "
+                             "an Azure instance can use use_internal_ips=True.")
+    else:
+
+        # Need to create a new virtual network
+        if get_workspace_virtual_network_name(config, network_client) is None:
+            virtual_network = create_virtual_network(config, resource_client, network_client)
+            virtual_network_name = virtual_network.name
+        else:
+            cli_logger.abort("There is a existing Virtual Network with the same name: {}, "
+                             "if you want to create a new workspace with the same name, "
+                             "you need to execute workspace delete first!".format(workspace_name))
+    return virtual_network_name
+
+
+def create_virtual_network(config, resource_client, network_client):
+    virtual_network_name = 'cloudtik-{}-vnet'.format(config["workspace_name"])
+    use_internal_ips = config["provider"].get("use_internal_ips", False)
+    resource_group_name = get_resource_group_name(config, resource_client, use_internal_ips)
+    assert "location" in config["provider"], (
+        "Provider config must include location field")
+
+    # choose a random subnet, skipping most common value of 0
+    random.seed(virtual_network_name)
+
+    params = {
+        "address_space": {
+            "address_prefixes": [
+                "10.{}.0.0/16".format(random.randint(1, 254))
+            ]
+        },
+        "location": config["provider"]["location"]
+    }
+    cli_logger.print("Creating workspace Virtual Network: {} on Azure...", virtual_network_name)
+    # create virtual network
+    try:
+        virtual_network = network_client.virtual_networks.begin_create_or_update(
+            resource_group_name=resource_group_name,
+            virtual_network_name=virtual_network_name, parameters=params).result()
+        # time.sleep(20)
+        cli_logger.print("Successfully created workspace Virtual Network: cloudtik-{}-vnet.".
+                         format(config["workspace_name"]))
+        return virtual_network
+    except Exception as e:
+        cli_logger.error(
+            "Failed to create workspace Virtual Network. {}", str(e))
+        raise e
+
+
 def _configure_network_resources(config, current_step, total_steps):
+    network_client = construct_network_client(config)
     resource_client = construct_resource_client(config)
-    # create resource_group
+    # create virtual network
     with cli_logger.group(
-            "Creating Resource Group",
+            "Creating Virtual Network",
             _numbered=("[]", current_step, total_steps)):
         current_step += 1
-        _create_resource_group(config, resource_client)
+        virtual_network = _create_vnet(config, resource_client, network_client)
 
-    # create vpc
-    # with cli_logger.group(
-    #         "Creating VPC",
-    #         _numbered=("[]", current_step, total_steps)):
-    #     current_step += 1
-    #     VpcId = _create_vpc(config, compute)
-    #
     # # create subnets
     # with cli_logger.group(
     #         "Creating subnets",
@@ -423,8 +600,17 @@ def construct_resource_client(config):
     if subscription_id is None:
         subscription_id = get_cli_profile().get_subscription_id()
     credential = AzureCliCredential()
-    resource_client = ResourceManagementClient(credential,
-                                               subscription_id)
+    resource_client = ResourceManagementClient(credential, subscription_id)
     logger.info("Using subscription id: %s", subscription_id)
     return resource_client
+
+
+def construct_network_client(config):
+    subscription_id = config["provider"].get("subscription_id")
+    if subscription_id is None:
+        subscription_id = get_cli_profile().get_subscription_id()
+    credential = AzureCliCredential()
+    network_client = NetworkManagementClient(credential, subscription_id)
+    logger.info("Using subscription id: %s", subscription_id)
+    return network_client
 

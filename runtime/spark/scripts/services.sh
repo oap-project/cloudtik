7a65b3b66e340f7af06c6ccb6d4751bc4a376aca
Haifeng Chen <haifeng.chen@intel.com>
2022-04-14 16:58:55 +0800

    Configure HDFS based on ENABLE_HDFS environment variable

--- runtime/spark/scripts/services.sh
+++ runtime/spark/scripts/services.sh
@@ -7,6 +7,9 @@ fi
 
 case "$1" in
   start-head)
+    if [ $ENABLE_HDFS == "true" ]; then
+        $HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
+    fi
     $HADOOP_HOME/bin/yarn --daemon start resourcemanager
     $SPARK_HOME/sbin/start-history-server.sh
     which jupyter && nohup jupyter lab  --no-browser --ip=* > jupyterlab.log 2>&1 &
@@ -17,17 +20,26 @@ case "$1" in
   stop-head)
     $HADOOP_HOME/bin/yarn --daemon stop resourcemanager
     $SPARK_HOME/sbin/stop-history-server.sh
+    if [ $ENABLE_HDFS == "true" ]; then
+        $HADOOP_HOME/sbin/hadoop-daemon.sh stop namenode
+    fi
     which jupyter && jupyter lab stop
     sudo service ganglia-monitor stop
     sudo service gmetad stop
     sudo service apache2 stop
     ;;
   start-worker)
+    if [ $ENABLE_HDFS == "true" ]; then
+        $HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
+    fi
     $HADOOP_HOME/bin/yarn --daemon start nodemanager
     sudo service ganglia-monitor start
     ;;
   stop-worker)
     $HADOOP_HOME/bin/yarn --daemon stop nodemanager
+    if [ $ENABLE_HDFS == "true" ]; then
+        $HADOOP_HOME/sbin/hadoop-daemon.sh stop datanode
+    fi
     sudo service ganglia-monitor stop
     ;;
   -h|--help)

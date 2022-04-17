3365a25ad764f98a6de22b511fc3e1c46f4b6d1e
Chen Haifeng <haifeng.chen@intel.com>
2022-04-17 10:12:50 +0800

    Fix the problem in gcs verification (#207)

--- python/cloudtik/providers/_private/_azure/utils.py
+++ python/cloudtik/providers/_private/_azure/utils.py
@@ -3,20 +3,20 @@ def get_azure_config(provider_config):
 
     azure_storage_kind = provider_config.get("azure_cloud_storage", {}).get("azure.storage.kind")
     if azure_storage_kind:
-        dict["AZURE_STORAGE_KIND"] = azure_storage_kind
+        config_dict["AZURE_STORAGE_KIND"] = azure_storage_kind
 
     azure_storage_account = provider_config.get("azure_cloud_storage", {}).get("azure.storage.account")
     if azure_storage_account:
-        dict["AZURE_STORAGE_ACCOUNT"] = azure_storage_account
+        config_dict["AZURE_STORAGE_ACCOUNT"] = azure_storage_account
 
     azure_container = provider_config.get("azure_cloud_storage", {}).get(
             "azure.container")
     if azure_container:
-        dict["AZURE_CONTAINER"] = azure_container
+        config_dict["AZURE_CONTAINER"] = azure_container
 
     azure_account_key = provider_config.get("azure_cloud_storage", {}).get(
             "azure.account.key")
     if azure_account_key:
-        dict["AZURE_ACCOUNT_KEY"] = azure_account_key
+        config_dict["AZURE_ACCOUNT_KEY"] = azure_account_key
 
     return config_dict
--- python/cloudtik/providers/_private/gcp/utils.py
+++ python/cloudtik/providers/_private/gcp/utils.py
@@ -3,25 +3,25 @@ def get_gcs_config(provider_config):
 
     project_id = provider_config.get("project_id")
     if project_id:
-        dict["PROJECT_ID"] = project_id
+        config_dict["PROJECT_ID"] = project_id
 
     gcp_gcs_bucket = provider_config.get("gcp_cloud_storage", {}).get("gcp.gcs.bucket")
     if gcp_gcs_bucket:
-        dict["GCP_GCS_BUCKET"] = gcp_gcs_bucket
+        config_dict["GCP_GCS_BUCKET"] = gcp_gcs_bucket
 
     gs_account_email = provider_config.get("gcp_cloud_storage", {}).get(
         "fs.gs.auth.service.account.email")
     if gs_account_email:
-        dict["FS_GS_AUTH_SERVICE_ACCOUNT_EMAIL"] = gs_account_email
+        config_dict["FS_GS_AUTH_SERVICE_ACCOUNT_EMAIL"] = gs_account_email
 
     gs_private_key_id = provider_config.get("gcp_cloud_storage", {}).get(
         "fs.gs.auth.service.account.private.key.id")
     if gs_private_key_id:
-        dict["FS_GS_AUTH_SERVICE_ACCOUNT_PRIVATE_KEY_ID"] = gs_private_key_id
+        config_dict["FS_GS_AUTH_SERVICE_ACCOUNT_PRIVATE_KEY_ID"] = gs_private_key_id
 
     gs_private_key = provider_config.get("gcp_cloud_storage", {}).get(
         "fs.gs.auth.service.account.private.key")
     if gs_private_key:
-        dict["FS_GS_AUTH_SERVICE_ACCOUNT_PRIVATE_KEY"] = gs_private_key
+        config_dict["FS_GS_AUTH_SERVICE_ACCOUNT_PRIVATE_KEY"] = gs_private_key
 
     return config_dict

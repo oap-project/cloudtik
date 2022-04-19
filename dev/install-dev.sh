273ea0f74de894b1a2ab69b8c13524ec62111eb7
Haifeng Chen <haifeng.chen@intel.com>
2022-04-19 19:34:46 +0800

    Update the commit sha before building wheel

--- dev/install-dev.sh
+++ dev/install-dev.sh
@@ -2,6 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # This script works for ubuntu
-sudo apt-get update && sudo apt-get install -y curl unzip cmake gcc g++
+sudo apt-get update && sudo apt-get install -y curl unzip sed cmake gcc g++
 
 bash ${SCRIPT_DIR}/install-conda.sh

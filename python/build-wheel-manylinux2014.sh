f6844dfcb834d4ff50ffa0d3e69a986db19297b0
Chen Haifeng <haifeng.chen@intel.com>
2022-04-20 14:28:09 +0800

    Move the the script for update commit to build.sh (#247)

--- python/build-wheel-manylinux2014.sh
+++ python/build-wheel-manylinux2014.sh
@@ -11,19 +11,6 @@ python -m pip install --upgrade pip
 # Make sure you have the latest version of PyPA’s build installed:
 python -m pip install --upgrade build
 
-# Update the commit sha
-if [ ! -n "${TRAVIS_COMMIT}" ]; then
-    CLOUDTIK_COMMIT_SHA=$TRAVIS_COMMIT
-fi
-
-if [ ! -n "$CLOUDTIK_COMMIT_SHA" ]; then
-    CLOUDTIK_COMMIT_SHA=$(which git >/dev/null && git rev-parse HEAD)
-fi
-
-if [ ! -z "$CLOUDTIK_COMMIT_SHA" ]; then
-    sed -i.bak "s/__commit__ = \".*\"/__commit__ = \"$CLOUDTIK_COMMIT_SHA\"/g" ./cloudtik/__init__.py && rm ./cloudtik/__init__.py.bak
-fi
-
 python -m pip install --upgrade twine
 
 # Build CloudTik wheel, at the same directory where pyproject.toml and setup.py locate:

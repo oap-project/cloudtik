3d4e59f782a4922ac7d7456c903e145bdb16b38c
Haifeng Chen <haifeng.chen@intel.com>
2022-04-23 12:37:16 +0800

    Implement HDFS as separate runtime

--- python/setup.py
+++ python/setup.py
@@ -347,7 +347,8 @@ setuptools.setup(
         "console_scripts": [
             "cloudtik=cloudtik.scripts.scripts:main",
             "cloudtik-ganglia=cloudtik.runtime.ganglia.scripts:main",
-            "cloudtik-spark=cloudtik.runtime.spark.scripts:main"
+            "cloudtik-spark=cloudtik.runtime.spark.scripts:main",
+            "cloudtik-hdfs=cloudtik.runtime.hdfs.scripts:main"
         ]
     },
     include_package_data=True,

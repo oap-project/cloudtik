553a4fb90e95a1644300f8774197d1a8df07d3b6
Chen Haifeng <haifeng.chen@intel.com>
2022-04-25 19:07:23 +0800

    Implementation of presto runtime (#326)

---
+++ python/cloudtik/runtime/presto/scripts/configure.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+args=$(getopt -a -o h::p: -l head::,node_ip_address::,head_address:: -- "$@")
+eval set -- "${args}"
+
+IS_HEAD_NODE=false
+USER_HOME=/home/$(whoami)
+RUNTIME_PATH=$USER_HOME/runtime
+
+while true
+do
+    case "$1" in
+    --head)
+        IS_HEAD_NODE=true
+        ;;
+    --node_ip_address)
+        NODE_IP_ADDRESS=$2
+        shift
+        ;;
+    -h|--head_address)
+        HEAD_ADDRESS=$2
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
+done
+
+function prepare_base_conf() {
+    source_dir=$(cd $(dirname ${BASH_SOURCE[0]})/..;pwd)/conf
+    output_dir=/tmp/presto/conf
+    rm -rf  $output_dir
+    mkdir -p $output_dir
+    cp -r $source_dir/* $output_dir
+}
+
+function check_presto_installed() {
+    if [ ! -n "${PRESTO_HOME}" ]; then
+        echo "Presto is not installed for PRESTO_HOME environment variable is not set."
+        exit 1
+    fi
+}
+
+function set_head_address() {
+    if [ $IS_HEAD_NODE == "true" ]; then
+        if [ ! -n "${NODE_IP_ADDRESS}" ]; then
+            HEAD_ADDRESS=$(hostname -I | awk '{print $1}')
+        else
+            HEAD_ADDRESS=${NODE_IP_ADDRESS}
+        fi
+    else
+        if [ ! -n "${HEAD_ADDRESS}" ]; then
+            # Error: no head address passed
+            echo "Error: head ip address should be passed."
+            exit 1
+        fi
+    fi
+}
+
+function update_presto_data_disks_config() {
+    presto_data_dir=""
+    if [ -d "/mnt/cloudtik" ]; then
+        for data_disk in /mnt/cloudtik/*; do
+            [ -d "$data_disk" ] || continue
+            if [ -z "$presto_data_dir" ]; then
+                presto_data_dir=$data_disk/presto/data
+            fi
+        done
+    fi
+
+    # if no disks mounted on /mnt/cloudtik
+    if [ -z "$presto_data_dir" ]; then
+        presto_data_dir="${RUNTIME_PATH}/shared/data"
+    fi
+
+    mkdir -p $presto_data_dir
+    sed -i "s!{%node.data-dir%}!${presto_data_dir}!g" $output_dir/presto/node.properties
+}
+
+function configure_presto() {
+    prepare_base_conf
+
+    cd $output_dir
+    node_id=$(uuid)
+    presto_log_dir=${PRESTO_HOME}/logs
+    mkdir -p ${presto_log_dir}
+    sed -i "s/{%HEAD_ADDRESS%}/${HEAD_ADDRESS}/g" `grep "{%HEAD_ADDRESS%}" -rl ./`
+    sed -i "s/{%node.environment%}/presto/g" $output_dir/presto/node.properties
+    sed -i "s/{%node.id%}/${node_id}/g" $output_dir/presto/node.properties
+    sed -i "s/{%node.log-dir%}${presto_log_dir}/g" $output_dir/presto/node.properties
+
+    update_presto_data_disks_config
+
+    if [ $IS_HEAD_NODE == "true" ]; then
+        cp ${output_dir}/presto/config.properties  ${HADOOP_HOME}/etc/config.properties
+    else
+        cp ${output_dir}/presto/config.worker.properties  ${HADOOP_HOME}/etc/config.properties
+    fi
+
+    cp ${output_dir}/presto/jvm.config  ${PRESTO_HOME}/etc/jvm.config
+    cp ${output_dir}/presto/node.properties  ${PRESTO_HOME}/etc/node.properties
+}
+
+check_presto_installed
+set_head_address
+configure_presto
+
---
+++ python/cloudtik/runtime/presto/utils.py
@@ -0,0 +1,70 @@
+import os
+from typing import Any, Dict
+
+from cloudtik.core._private.utils import merge_rooted_config_hierarchy, _get_runtime_config_object
+
+RUNTIME_PROCESSES = [
+    # The first element is the substring to filter.
+    # The second element, if True, is to filter ps results by command name.
+    # The third element is the process name.
+    # The forth element, if node, the process should on all nodes,if head, the process should on head node.
+    ["presto-server", False, "PrestoServer", "node"],
+]
+
+RUNTIME_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def _config_runtime_resources(cluster_config: Dict[str, Any]) -> Dict[str, Any]:
+    return cluster_config
+
+
+def _get_runtime_processes():
+    return RUNTIME_PROCESSES
+
+
+def _is_runtime_scripts(script_file):
+    return False
+
+
+def _get_runnable_command(target):
+    return None
+
+
+def _with_runtime_environment_variables(runtime_config, provider):
+    runtime_envs = {"PRESTO_ENABLED": True}
+    return runtime_envs
+
+
+def _get_runtime_logs():
+    logs_dir = os.path.join(os.getenv("PRESTO_HOME"), "logs")
+    all_logs = {"presto": logs_dir}
+    return all_logs
+
+
+def _validate_config(config: Dict[str, Any], provider):
+    pass
+
+
+def _verify_config(config: Dict[str, Any], provider):
+    pass
+
+
+def _get_config_object(cluster_config: Dict[str, Any], object_name: str) -> Dict[str, Any]:
+    config_root = os.path.join(RUNTIME_ROOT_PATH, "config")
+    runtime_commands = _get_runtime_config_object(config_root, cluster_config["provider"], object_name)
+    return merge_rooted_config_hierarchy(config_root, runtime_commands, object_name)
+
+
+def _get_runtime_commands(cluster_config: Dict[str, Any]) -> Dict[str, Any]:
+    return _get_config_object(cluster_config, "commands")
+
+
+def _get_defaults_config(cluster_config: Dict[str, Any]) -> Dict[str, Any]:
+    return _get_config_object(cluster_config, "defaults")
+
+
+def _get_useful_urls(cluster_head_ip):
+    urls = [
+        {"name": "Presto Web UI", "url": "http://{}:8080".format(cluster_head_ip)},
+    ]
+    return urls

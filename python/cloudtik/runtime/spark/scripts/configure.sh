42ed4a00c10c7aa1867606fa68628309150b9316
Chen Haifeng <haifeng.chen@intel.com>
2022-04-26 14:32:52 +0800

    Spark hive metastore jars (#337)
    * Allow to install hive for metastore jars
    
    * Configure metastore only for head for spark

--- python/cloudtik/runtime/spark/scripts/configure.sh
+++ python/cloudtik/runtime/spark/scripts/configure.sh
@@ -267,9 +267,15 @@ function update_metastore_config() {
     SPARK_DEFAULTS=${output_dir}/spark/spark-defaults.conf
     if [ "$METASTORE_ENABLED" == "true" ];then
         METASTORE_IP=${HEAD_ADDRESS}
+
         hive_metastore_uris="thrift://${METASTORE_IP}:9083"
         hive_metastore_version="3.1.2"
-        hive_metastore_jars="${METASTORE_HOME}/lib/*"
+
+        if [ ! -n "${HIVE_HOME}" ]; then
+            hive_metastore_jars=maven
+        else
+            hive_metastore_jars="${HIVE_HOME}/lib/*"
+
         sed -i "s!{%spark.hadoop.hive.metastore.uris%}!spark.hadoop.hive.metastore.uris ${hive_metastore_uris}!g" ${SPARK_DEFAULTS}
         sed -i "s!{%spark.sql.hive.metastore.version%}!spark.sql.hive.metastore.version ${hive_metastore_version}!g" ${SPARK_DEFAULTS}
         sed -i "s!{%spark.sql.hive.metastore.jars%}!spark.sql.hive.metastore.jars ${hive_metastore_jars}!g" ${SPARK_DEFAULTS}
@@ -289,7 +295,6 @@ function configure_hadoop_and_spark() {
 
     update_spark_runtime_config
     update_data_disks_config
-    update_metastore_config
 
     if [ "$HDFS_ENABLED" == "true" ];then
         update_config_for_hdfs
@@ -301,6 +306,8 @@ function configure_hadoop_and_spark() {
     cp -r ${output_dir}/hadoop/yarn-site.xml  ${HADOOP_HOME}/etc/hadoop/
 
     if [ $IS_HEAD_NODE == "true" ];then
+        update_metastore_config
+
         cp -r ${output_dir}/spark/*  ${SPARK_HOME}/conf
 
         if [ "$HDFS_ENABLED" == "true" ]; then

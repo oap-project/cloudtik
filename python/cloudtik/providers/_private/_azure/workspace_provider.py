7e08baf79759c63fffa2f7028f5bc6424cec15cb
Chen Haifeng <haifeng.chen@intel.com>
2022-04-29 10:22:07 +0800

    Correct the config name for workspace provider (#365)

--- python/cloudtik/providers/_private/_azure/workspace_provider.py
+++ python/cloudtik/providers/_private/_azure/workspace_provider.py
@@ -12,17 +12,17 @@ class AzureWorkspaceProvider(WorkspaceProvider):
     def __init__(self, provider_config, workspace_name):
         WorkspaceProvider.__init__(self, provider_config, workspace_name)
 
-    def create_workspace(self, cluster_config):
-        create_azure_workspace(cluster_config)
+    def create_workspace(self, config):
+        create_azure_workspace(config)
 
-    def delete_workspace(self, cluster_config):
-        delete_workspace_azure(cluster_config)
+    def delete_workspace(self, config):
+        delete_workspace_azure(config)
     
-    def update_workspace_firewalls(self, cluster_config):
-        update_azure_workspace_firewalls(cluster_config)
+    def update_workspace_firewalls(self, config):
+        update_azure_workspace_firewalls(config)
     
-    def check_workspace_resource(self, cluster_config):
-        return check_azure_workspace_resource(cluster_config)
+    def check_workspace_resource(self, config):
+        return check_azure_workspace_resource(config)
 
     @staticmethod
     def validate_config(
--- python/cloudtik/providers/_private/aws/workspace_provider.py
+++ python/cloudtik/providers/_private/aws/workspace_provider.py
@@ -14,17 +14,17 @@ class AWSWorkspaceProvider(WorkspaceProvider):
     def __init__(self, provider_config, workspace_name):
         WorkspaceProvider.__init__(self, provider_config, workspace_name)
 
-    def create_workspace(self, cluster_config):
-        create_aws_workspace(cluster_config)
+    def create_workspace(self, config):
+        create_aws_workspace(config)
 
-    def delete_workspace(self, cluster_config):
-        delete_workspace_aws(cluster_config)
+    def delete_workspace(self, config):
+        delete_workspace_aws(config)
 
-    def update_workspace_firewalls(self, cluster_config):
-        update_aws_workspace_firewalls(cluster_config)
+    def update_workspace_firewalls(self, config):
+        update_aws_workspace_firewalls(config)
 
-    def check_workspace_resource(self, cluster_config):
-        return check_aws_workspace_resource(cluster_config)
+    def check_workspace_resource(self, config):
+        return check_aws_workspace_resource(config)
 
     def publish_global_variables(self, cluster_config: Dict[str, Any],
                                  head_node_id: str, global_variables: Dict[str, Any]):

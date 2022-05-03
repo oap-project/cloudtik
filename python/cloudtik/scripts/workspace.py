cacea33bf6e76bb44e3d54782110ef67ccb4e1fd
Haifeng Chen <haifeng.chen@intel.com>
2022-05-03 12:12:35 +0800

    Implement workspace in API

--- python/cloudtik/scripts/workspace.py
+++ python/cloudtik/scripts/workspace.py
@@ -3,7 +3,7 @@ import logging
 import urllib
 
 from cloudtik.core._private.workspace.workspace_operator import (
-    create_or_update_workspace, delete_workspace, update_workspace_firewalls)
+    create_workspace, delete_workspace, update_workspace_firewalls)
 from cloudtik.core._private.cli_logger import (add_click_logging_options, cli_logger)
 from cloudtik.scripts.utils import NaturalOrderGroup
 
@@ -32,12 +32,12 @@ def workspace():
     type=str,
     help="Override the configured workspace name.")
 @click.option(
-    "--no-workspace-config-cache",
+    "--no-config-cache",
     is_flag=True,
     default=False,
     help="Disable the local workspace config cache.")
 @add_click_logging_options
-def create(workspace_config_file, yes, workspace_name, no_workspace_config_cache):
+def create(workspace_config_file, yes, workspace_name, no_config_cache):
     """Create a workspace on cloud using the workspace configuration file."""
     if urllib.parse.urlparse(workspace_config_file).scheme in ("http", "https"):
         try:
@@ -52,11 +52,11 @@ def create(workspace_config_file, yes, workspace_name, no_workspace_config_cache
             cli_logger.warning(
                 "Could not download remote cluster configuration file.")
 
-    create_or_update_workspace(
+    create_workspace(
         config_file=workspace_config_file,
         yes=yes,
         override_workspace_name=workspace_name,
-        no_workspace_config_cache=no_workspace_config_cache)
+        no_config_cache=no_config_cache)
 
 
 @workspace.command()
@@ -97,7 +97,6 @@ def delete(workspace_config_file, yes, workspace_name):
 def update_firewalls(workspace_config_file, yes, workspace_name):
     """Update the firewalls for workspace."""
     update_workspace_firewalls(workspace_config_file, yes, workspace_name)
-
 
 
 # core commands working on workspace

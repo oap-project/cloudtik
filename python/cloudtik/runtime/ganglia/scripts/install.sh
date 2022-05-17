d3461e32894998f8e55747b96f3dae282900812e
haojinIntel <hao.jin@intel.com>
2022-05-17 15:55:12 +0800

    Support modpython for ganglia on Ubuntu20.04 (#468)
    * Support modpython for ganglia on Ubuntu20.04
    
    * Don't print message when installing ganglia_monitor_python
    
    * Avoid compiling ganglia-monitor-core during setting up cluster
    
    * Small update
    
    * Keep modpython.so under python/dist folder

--- python/cloudtik/runtime/ganglia/scripts/install.sh
+++ python/cloudtik/runtime/ganglia/scripts/install.sh
@@ -20,16 +20,24 @@ do
 done
 
 
+function install_ganglia_monitor_python() {
+    wget -q --show-progress -P /tmp https://d30257nes7d4fq.cloudfront.net/downloads/cloudtik/ganglia/modpython.so
+    sudo cp /tmp/modpython.so /usr/lib/ganglia/ && sudo chmod 644 /usr/lib/ganglia/modpython.so
+    sudo apt-get install -y  ganglia-monitor-python python2.7-dev > /dev/null
+}
+
 function install_ganglia_server() {
     # Simply do the install, if they are already installed, it doesn't take time
     sudo apt-get -qq update -y > /dev/null
     sudo apt-get -qq install -y apache2 php libapache2-mod-php php-common php-mbstring php-gmp php-curl php-intl php-xmlrpc php-zip php-gd php-mysql php-xml > /dev/null
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y ganglia-monitor rrdtool gmetad ganglia-webfrontend > /dev/null
+    install_ganglia_monitor_python
 }
 
 function install_ganglia_client() {
     sudo apt-get -qq update -y > /dev/null
     sudo apt-get -qq install -y ganglia-monitor > /dev/null
+    install_ganglia_monitor_python
 }
 
 function install_ganglia() {

0e3e754c8f5960c20d9ef0ebcc255ffc3bd47c04
Qing Yao <qing.yao@intel.com>
2022-05-18 22:51:45 +0800

    Support delete and check vpc endpoint for s3 in AWS workspace.

--- python/cloudtik/providers/_private/aws/config.py
+++ python/cloudtik/providers/_private/aws/config.py
@@ -60,7 +60,7 @@ DEFAULT_AMI = {
 }
 
 NUM_AWS_WORKSPACE_CREATION_STEPS = 8
-NUM_AWS_WORKSPACE_DELETION_STEPS = 7
+NUM_AWS_WORKSPACE_DELETION_STEPS = 8
 
 # todo: cli_logger should handle this assert properly
 # this should probably also happens somewhere else
@@ -336,6 +336,12 @@ def get_vpc_internat_gateways(ec2, vpc_id):
     return igws
 
 
+def get_vpc_endpoint_for_s3(ec2_client, workspace_name):
+    vpc_endpoint = ec2_client.describe_vpc_endpoints(Filters=[
+        {'Name': 'tag:Name', 'Values': ['cloudtik-{}-vpc-endpoint-s3'.format(workspace_name)]}])
+    return vpc_endpoint['VpcEndpoints']
+
+
 def check_aws_workspace_resource(config):
     ec2 = _resource("ec2", config)
     ec2_client = _client("ec2", config)
@@ -350,6 +356,7 @@ def check_aws_workspace_resource(config):
          5.) Check route-tables
          6.) Check Internat-gateways
          7.) Check security-group
+         8.) Check VPC endpoint for s3
     """
     if vpc_id is None:
         return False
@@ -364,6 +371,8 @@ def check_aws_workspace_resource(config):
     if len(get_vpc_internat_gateways(ec2, vpc_id)) == 0:
         return False
     if get_workspace_security_group(config, vpc_id, workspace_name) is None:
+        return False
+    if len(get_vpc_endpoint_for_s3(ec2_client, workspace_name)) == 0:
         return False
     return True
 
@@ -500,6 +509,13 @@ def _delete_network_resources(config, workspace_name,
             _numbered=("[]", current_step, total_steps)):
         current_step += 1
         _delete_security_group(config, vpc_id)
+
+    # delete vpc endpoint for s3
+    with cli_logger.group(
+            "Deleting VPC endpoint for S3",
+            _numbered=("[]", current_step, total_steps)):
+        current_step += 1
+        _delete_vpc_endpoint_for_s3(ec2_client, workspace_name)
 
     # delete vpc
     if not use_internal_ips:
@@ -1038,6 +1054,21 @@ def _delete_vpc(ec2, ec2_client, vpc_id):
     return
 
 
+def _delete_vpc_endpoint_for_s3(ec2_client, workspace_name):
+    endpoint_ids = [endpoint['VpcEndpointId'] for endpoint in get_vpc_endpoint_for_s3(ec2_client, workspace_name)]
+    if len(endpoint_ids) == 0:
+        cli_logger.print("This VPC endpoint for S3 doesn't exist. ")
+        return
+    try:
+        cli_logger.print("Deleting VPC endpoint for S3.")
+        ec2_client.delete_vpc_endpoints(
+                        VpcEndpointIds=endpoint_ids
+                        )
+    except Exception as e:
+        cli_logger.error("Failed to delete VPC endpoint for S3. {}", str(e))
+        raise e
+    return
+
 def _create_vpc(config,  ec2):
     cli_logger.print("Creating workspace VPC...")
     # create vpc
@@ -1160,20 +1191,14 @@ def _create_vpc_endpoint_for_s3(config, ec2, ec2_client, vpc):
             VpcId=vpc.id,
             ServiceName='com.amazonaws.{}.s3'.format(region),
             RouteTableIds=route_table_ids,
-            Tags=[{'Key': 'Name', 'Value': 'cloudtik-{}-vpc-endpoint-s3'.format(config["workspace_name"])}],
+            TagSpecifications=[{'ResourceType': "vpc-endpoint", "Tags": [{'Key': 'Name',
+                                                                          'Value': 'cloudtik-{}-vpc-endpoint-s3'.format(config["workspace_name"])
+                                                                          }]}],
         )
 
     except Exception as e:
         cli_logger.error("Failed to create  Vpc Endpoint for S3. {}", str(e))
-        try:
-            cli_logger.print("Try to find the existing Vpc Endpoint for S3...")
-            vpc_endpoint = ec2_client.describe_vpc_endpoints(Filters=[
-                                                                {'Name': 'tag:Name', 'Values': 'cloudtik-{}-vpc-endpoint-s3'.format(config["workspace_name"])}])
-            cli_logger.print(
-                "Found an existing Vpc Endpoint for S3. Will use this one")
-        except Exception:
-            raise e
-    return vpc_endpoint
+        raise e
 
 
 def _update_route_table_for_public_subnet(config, ec2, ec2_client, vpc, subnet, igw):

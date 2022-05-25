0deb84472d555e14ac7c6c70d5e64b88e6ab7c02
Qing Yao <qing.yao@intel.com>
2022-05-26 04:39:18 +0800

    Support api test can run multiple config files at the same time.

--- python/cloudtik/tests/core/conftest.py
+++ python/cloudtik/tests/core/conftest.py
@@ -1,34 +1,47 @@
 import pytest
 import os
 
+import yaml
 
 EXAMPLE_PATH = os.path.abspath(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))))
-AWS_CLUSTER_EXAMPLE_PATH = os.path.join(
+AWS_CLUSTER_EXAMPLE_PATHS = os.path.join(
     EXAMPLE_PATH, "example/cluster/aws/example-docker.yaml")
-AWS_WORKSPACE_EXAMPLE_PATH = os.path.join(
+AWS_WORKSPACE_EXAMPLE_PATHS = os.path.join(
     EXAMPLE_PATH, "example/cluster/aws/example-workspace.yaml")
 
 
-def workspace_conf():
-    return AWS_WORKSPACE_EXAMPLE_PATH
+def workspace_conf_files():
+    return AWS_WORKSPACE_EXAMPLE_PATHS.split(",")
 
 
-def cluster_conf():
-    return AWS_CLUSTER_EXAMPLE_PATH
+def cluster_conf_files():
+    return AWS_CLUSTER_EXAMPLE_PATHS.split(",")
 
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--workspace_conf", action="store", default=AWS_WORKSPACE_EXAMPLE_PATH
+        "--workspace_conf", action="store", default=AWS_WORKSPACE_EXAMPLE_PATHS
     )
     parser.addoption(
-        "--cluster_conf", action="store", default=AWS_CLUSTER_EXAMPLE_PATH
+        "--cluster_conf", action="store", default=AWS_CLUSTER_EXAMPLE_PATHS
     )
 
 
 def pytest_configure(config):
-    global AWS_CLUSTER_EXAMPLE_PATH
-    global AWS_WORKSPACE_EXAMPLE_PATH
-    AWS_WORKSPACE_EXAMPLE_PATH = config.getoption("--workspace_conf")
-    AWS_CLUSTER_EXAMPLE_PATH = config.getoption("--cluster_conf")
+    global AWS_CLUSTER_EXAMPLE_PATHS
+    global AWS_WORKSPACE_EXAMPLE_PATHS
+    AWS_WORKSPACE_EXAMPLE_PATHS = config.getoption("--workspace_conf")
+    AWS_CLUSTER_EXAMPLE_PATHS = config.getoption("--cluster_conf")
+
+@pytest.fixture(scope="class", params=cluster_conf_files())
+def cluster_config(request):
+    print('setup once per each param', request.param)
+    config = yaml.safe_load(open(request.param).read())
+    return config
+
+@pytest.fixture(scope="class", params=workspace_conf_files())
+def workspace_config(request):
+    print('setup once per each param', request.param)
+    config = yaml.safe_load(open(request.param).read())
+    return config
--- python/cloudtik/tests/core/test_api.py
+++ python/cloudtik/tests/core/test_api.py
@@ -1,83 +1,67 @@
 import os
-import yaml
 import pytest
-
-from conftest import workspace_conf, cluster_conf
 
 from cloudtik.core.api import Workspace, Cluster
 
 
 class TestWorkspace:
-    @classmethod
-    def setup_class(self):
-        config_file = workspace_conf()
-        config = yaml.safe_load(open(config_file).read())
-        self.workspace = Workspace(config)
 
-    def test_create(self):
-        res = self.workspace.create()
+    def test_create(self, workspace_config):
         # TODO The return value of each operation should be standard
-        assert res == 0
+        res = Workspace(workspace_config).create()
 
-    def test_update_firewalls(self):
-        res = self.workspace.update_firewalls()
-        assert res == 0
+    def test_update_firewalls(self, workspace_config):
+        res = Workspace(workspace_config).update_firewalls()
 
-    def test_list_clusters(self):
-        res = self.workspace.list_clusters()
+    def test_list_clusters(self, workspace_config):
+        res = Workspace(workspace_config).list_clusters()
         assert type(res) == dict
 
-    def test_delete(self):
-        res = self.workspace.delete()
-        assert res == 0
+    def test_delete(self, workspace_config):
+        res = Workspace(workspace_config).delete()
 
 
 class TestCluster:
-    @classmethod
-    def setup_class(self):
-        self.config_file = cluster_conf()
-        config = yaml.safe_load(open(self.config_file).read())
-        self.cluster = Cluster(config)
 
-    def test_start(self):
-        res = self.cluster.start()
+    def test_start(self, cluster_config):
+        res = Cluster(cluster_config).start()
 
-    def test_wait_for_ready(self):
+    def test_wait_for_ready(self, cluster_config):
         timeout = 60 * 5
-        self.cluster.wait_for_ready(timeout=timeout)
+        Cluster(cluster_config).wait_for_ready(timeout=timeout)
 
-    def test_exec(self):
-        res = self.cluster.exec(cmd="uptime", all_nodes=True)
+    def test_exec(self, cluster_config):
+        res = Cluster(cluster_config).exec(cmd="uptime", all_nodes=True)
 
-    def test_rsync_up(self):
-        res = self.cluster.rsync(source=self.config_file, target="~/test.yaml", down=False)
+    def test_rsync_up(self, cluster_config):
+        res = Cluster(cluster_config).rsync(source=self.config_file, target="~/test.yaml", down=False)
 
-    def test_rsync_down(self):
+    def test_rsync_down(self, cluster_config):
         tmp_file = "/tmp/cloudtik_bootstrap_config.yaml"
-        res = self.cluster.rsync(source="~/cloudtik_bootstrap_config.yaml", target=tmp_file, down=True)
+        res = Cluster(cluster_config).rsync(source="~/cloudtik_bootstrap_config.yaml", target=tmp_file, down=True)
         file_exist = os.path.exists(tmp_file)
         if file_exist:
             os.remove(tmp_file)
         assert file_exist
 
-    def test_get_head_node_ip(self):
-        res = self.cluster.get_head_node_ip()
+    def test_get_head_node_ip(self, cluster_config):
+        res = Cluster(cluster_config).get_head_node_ip()
         assert type(res) == str
 
-    def test_get_worker_node_ips(self):
-        res = self.cluster.get_worker_node_ips()
+    def test_get_worker_node_ips(self, cluster_config):
+        res = Cluster(cluster_config).get_worker_node_ips()
         assert type(res) == list
 
-    def test_get_nodes(self):
-        res = self.cluster.get_nodes()
+    def test_get_nodes(self, cluster_config):
+        res = Cluster(cluster_config).get_nodes()
         assert type(res) == list
 
-    def test_get_info(self):
-        res = self.cluster.get_info()
+    def test_get_info(self, cluster_config):
+        res = Cluster(cluster_config).get_info()
         assert type(res) == dict
 
-    def test_stop(self):
-        res = self.cluster.stop()
+    def test_stop(self, cluster_config):
+        res = Cluster(cluster_config).stop()
 
 
 if __name__ == "__main__":

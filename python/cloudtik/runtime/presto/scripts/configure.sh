7a1743c976eda103594fcbdc93b9c67c7523c53e
Haifeng Chen <haifeng.chen@intel.com>
2022-05-31 16:50:34 +0800

    Presto doens't support datalake gen 2

--- python/cloudtik/runtime/presto/scripts/configure.sh
+++ python/cloudtik/runtime/presto/scripts/configure.sh
@@ -104,37 +104,24 @@ function update_storage_config_for_aws() {
 }
 
 function update_credential_config_for_azure() {
-    if [ "$AZURE_STORAGE_TYPE" == "blob" ];then
-        AZURE_ENDPOINT="blob"
-    else
-        # Default to datalake
-        AZURE_ENDPOINT="dfs"
-    fi
-
+    AZURE_ENDPOINT="blob"
     sed -i "s#{%azure.storage.account%}#${AZURE_STORAGE_ACCOUNT}#g" $catalog_dir/hive-azure-core-site.xml
     sed -i "s#{%storage.endpoint%}#${AZURE_ENDPOINT}#g" $catalog_dir/hive-azure-core-site.xml
-
     sed -i "s#{%azure.account.key%}#${AZURE_ACCOUNT_KEY}#g" $catalog_dir/hive-azure-core-site.xml
-    sed -i "s#{%fs.azure.account.oauth2.msi.tenant%}#${AZURE_MANAGED_IDENTITY_TENANT_ID}#g" $catalog_dir/hive-azure-core-site.xml
-    sed -i "s#{%fs.azure.account.oauth2.client.id%}#${AZURE_MANAGED_IDENTITY_CLIENT_ID}#g" $catalog_dir/hive-azure-core-site.xml
-
-    if [ "$AZURE_STORAGE_TYPE" != "blob" ];then
-        # datalake
-        if [ -n  "${AZURE_ACCOUNT_KEY}" ];then
-            sed -i "s#{%auth.type%}#SharedKey#g" $catalog_dir/hive-azure-core-site.xml
-        else
-            sed -i "s#{%auth.type%}##g" $catalog_dir/hive-azure-core-site.xml
-        fi
-    fi
 }
 
 function update_storage_config_for_azure() {
-    update_credential_config_for_azure
+    if [ "$AZURE_STORAGE_TYPE" == "blob" ];then
+        update_credential_config_for_azure
 
-    HIVE_AZURE_CORE_SITE="${PRESTO_HOME}/etc/catalog/hive-azure-core-site.xml"
-    cp $catalog_dir/hive-azure-core-site.xml ${HIVE_AZURE_CORE_SITE}
-    sed -i "s!{%hive.config.resources%}!${HIVE_AZURE_CORE_SITE}!g" $catalog_dir/hive.config.properties
-    cat $catalog_dir/hive.config.properties >> $catalog_dir/hive.properties
+        HIVE_AZURE_CORE_SITE="${PRESTO_HOME}/etc/catalog/hive-azure-core-site.xml"
+        cp $catalog_dir/hive-azure-core-site.xml ${HIVE_AZURE_CORE_SITE}
+        sed -i "s!{%hive.config.resources%}!${HIVE_AZURE_CORE_SITE}!g" $catalog_dir/hive.config.properties
+        cat $catalog_dir/hive.config.properties >> $catalog_dir/hive.properties
+    else
+        # datalake is not supported
+        echo "WARNING: Azure Data Lake Storage Gen 2 is not supported for this version."
+    fi
 }
 
 function update_storage_config_for_gcp() {

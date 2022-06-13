b24ffcf647f67cae2944fe184c7e3b56b59026cd
Qing Yao <qing.yao@intel.com>
2022-06-13 14:35:09 +0800

    Support bootstrap_oap.sh can accept parameter to specify the version of OAP.

--- tools/integrations/oap/bootstrap_oap.sh
+++ tools/integrations/oap/bootstrap_oap.sh
@@ -3,5 +3,11 @@
 export USER_HOME=/home/$(whoami)
 oap_install_dir=$USER_HOME/runtime/oap
 
+if [ ! -n "$1" ];then
+    oap_version=$1
+else
+    oap_version=1.3.1.h331
+fi
+
 # Install OAP by Conda
-conda create -p "${oap_install_dir}" -c conda-forge -c intel -y oap=1.3.1
+conda create -p "${oap_install_dir}"  -c conda-forge -c intel-beaver -c intel-bigdata -c intel -y oap=${oap_version}

d2ec1f758f85820b774ea011086e070a23dc468d
Chen Haifeng <haifeng.chen@intel.com>
2022-07-19 08:46:05 +0800

    Fix the wrong default redis password passed to controller in operator (#679)

--- python/cloudtik/providers/kubernetes/cloudtik_operator/operator.py
+++ python/cloudtik/providers/kubernetes/cloudtik_operator/operator.py
@@ -114,7 +114,7 @@ class CloudTikCluster:
         controller = cluster_controller.ClusterController(
             address,
             cluster_scaling_config=self.controller_config_path,
-            redis_password=constants.CLOUDTIK_DEFAULT_PORT,
+            redis_password=constants.CLOUDTIK_REDIS_DEFAULT_PASSWORD,
             prefix_cluster_info=True,
             stop_event=self.controller_stop_event,
             retry_on_failure=False,

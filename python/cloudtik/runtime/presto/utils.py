e414d7e6fd2de5961f6711d6d99a716e5b0e943d
haojinIntel <hao.jin@intel.com>
2022-07-20 16:21:06 +0800

    Fix the bug for presto and trino for branch-0.9

--- python/cloudtik/runtime/presto/utils.py
+++ python/cloudtik/runtime/presto/utils.py
@@ -136,7 +136,7 @@ def _get_defaults_config(runtime_config: Dict[str, Any],
 
 def _get_useful_urls(cluster_head_ip):
     urls = [
-        {"name": "Presto Web UI", "url": "http://{}:8080".format(cluster_head_ip)},
+        {"name": "Presto Web UI", "url": "http://{}:8081".format(cluster_head_ip)},
     ]
     return urls
 

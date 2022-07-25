a96eb5a75070af55a7659a5de0db517131b10a01
Qing Yao <qing.yao@intel.com>
2022-07-25 21:21:56 +0800

    Small fix for empty string check in configure.sh.

--- python/cloudtik/runtime/spark/scripts/configure.sh
+++ python/cloudtik/runtime/spark/scripts/configure.sh
@@ -112,7 +112,7 @@ function update_credential_config_for_aws() {
     sed -i "s#{%fs.s3a.access.key%}#${AWS_S3_ACCESS_KEY_ID}#g" `grep "{%fs.s3a.access.key%}" -rl ./`
 
     JCEKS_PATH="jceks://file@${HADOOP_HOME}/etc/hadoop/s3.jceks"
-    if [ -z "${AWS_S3_SECRET_ACCESS_KEY}" ]; then
+    if [ ! -z "${AWS_S3_SECRET_ACCESS_KEY}" ]; then
       ${HADOOP_HOME}/bin/hadoop credential create fs.s3a.secret.key -value ${AWS_S3_SECRET_ACCESS_KEY}  -provider ${JCEKS_PATH}
       sed -i "s#{%s3.jceks%}#${JCEKS_PATH}#g" `grep "{%s3.jceks%}" -rl ./`
     fi
@@ -125,7 +125,7 @@ function update_credential_config_for_gcp() {
     sed -i "s#{%fs.gs.auth.service.account.private.key.id%}#${GCS_SERVICE_ACCOUNT_PRIVATE_KEY_ID}#g" `grep "{%fs.gs.auth.service.account.private.key.id%}" -rl ./`
 
     JCEKS_PATH="jceks://file@${HADOOP_HOME}/etc/hadoop/gcp.jceks"
-    if [ -z "${GCS_SERVICE_ACCOUNT_PRIVATE_KEY}" ]; then
+    if [ ! -z "${GCS_SERVICE_ACCOUNT_PRIVATE_KEY}" ]; then
       ${HADOOP_HOME}/bin/hadoop credential create fs.gs.auth.service.account.private.key -value ${GCS_SERVICE_ACCOUNT_PRIVATE_KEY}  -provider ${JCEKS_PATH}
       sed -i "s#{%gcp.jceks%}#${JCEKS_PATH}#g" `grep "{%gcp.jceks%}" -rl ./`
     fi

5fbd891284b2a0efdd5bac6df07021ed4e07b488
Haifeng Chen <haifeng.chen@intel.com>
2022-08-17 16:51:31 +0800

    Add Spark native runtime which installs the native sql engine

--- tools/integrations/oap/bootstrap_oap.sh
+++ tools/integrations/oap/bootstrap_oap.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 export USER_HOME=/home/$(whoami)
-oap_install_dir=$USER_HOME/runtime/oap
+export RUNTIME_PATH=$USER_HOME/runtime
+export OAP_HOME=$RUNTIME_PATH/oap
 
 if [ -n "$1" ];then
-    oap_version=$1
+    OAP_VERSION=$1
 else
-    oap_version=1.4.0.spark32
+    OAP_VERSION=1.4.0.spark32
 fi
 
 # Install OAP by Conda
-conda create -p "${oap_install_dir}"  -c conda-forge -c intel-beaver -c intel-bigdata -c intel -y oap=${oap_version}
+if [ ! -d "${OAP_HOME}" ]; then
+    conda create -p "${OAP_HOME}" -c conda-forge -c intel-beaver -c intel-bigdata -c intel -y oap=${OAP_VERSION}
+fi

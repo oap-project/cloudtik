ec0f3b9c62c1ef129c3118c48995a5e502a05f7f
Haifeng Chen <haifeng.chen@intel.com>
2022-08-23 16:58:15 +0800

    Respect Spark version from the existing env variables

--- python/cloudtik/runtime/spark/scripts/install.sh
+++ python/cloudtik/runtime/spark/scripts/install.sh
@@ -19,8 +19,13 @@ do
     shift
 done
 
+if [ -z "$SPARK_VERSION" ]; then
+    # if SPARK_VERSION is not set, set a default Spark version
+    export SPARK_VERSION=3.2.1
+fi
+
+# Set Hadoop version based on Spark version
 export HADOOP_VERSION=3.3.1
-export SPARK_VERSION=3.2.1
 
 export USER_HOME=/home/$(whoami)
 export RUNTIME_PATH=$USER_HOME/runtime

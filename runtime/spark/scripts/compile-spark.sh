56fc57277c244bc9abeab9980edfda3ab558b404
haojinIntel <hao.jin@intel.com>
2022-09-01 17:12:47 +0800

    Update the patch for 0002-runtime-Filter.patch

--- runtime/spark/scripts/compile-spark.sh
+++ runtime/spark/scripts/compile-spark.sh
@@ -7,7 +7,7 @@ git clone https://github.com/apache/spark.git /tmp/spark
 cd /tmp/spark && git checkout v3.3.0
 
 git apply $CLOUDTIK_HOME/runtime/spark/optimization/0001-Top-N.patch
-git apply $CLOUDTIK_HOME/runtime/spark/optimization/0002-runtime-Filter.patch
+git apply $CLOUDTIK_HOME/runtime/spark/optimization/0002-Runtime-Filter.patch
 
 ./dev/make-distribution.sh --name hadoop3  --tgz  -Phadoop-3 -Dhadoop.version=3.3.1  -Phive -Phive-thriftserver  -Pyarn \
  -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true

d3ffa388f3fcbd15229988b7b88adb8a463a0a5d
Haifeng Chen <haifeng.chen@intel.com>
2022-09-30 11:09:54 +0800

    Handling multi-arch during installation

--- python/cloudtik/runtime/common/scripts/jdk-install.sh
+++ python/cloudtik/runtime/common/scripts/jdk-install.sh
@@ -5,10 +5,18 @@ function install_jdk() {
     export JAVA_HOME=$RUNTIME_PATH/jdk
 
     if [ ! -d "${JAVA_HOME}" ]; then
-      (cd $RUNTIME_PATH && wget -q --show-progress https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.16.1%2B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.16.1_1.tar.gz -O openjdk.tar.gz && \
-          mkdir -p "$JAVA_HOME" && \
-          tar --extract --file openjdk.tar.gz --directory "$JAVA_HOME" --strip-components 1 --no-same-owner && \
-          rm openjdk.tar.gz)
+        arch=$(uname -m)
+        if [ "${arch}" == "aarch64" ]; then
+            arch_jdk=${arch}
+        else
+            arch_jdk="x64"
+        fi
+        jdk_download_url="https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.16.1%2B1/OpenJDK11U-jdk_${arch_jdk}_linux_hotspot_11.0.16.1_1.tar.gz"
+
+        (cd $RUNTIME_PATH && wget -q --show-progress ${jdk_download_url} -O openjdk.tar.gz && \
+            mkdir -p "$JAVA_HOME" && \
+            tar --extract --file openjdk.tar.gz --directory "$JAVA_HOME" --strip-components 1 --no-same-owner && \
+            rm openjdk.tar.gz)
         echo "export JAVA_HOME=$JAVA_HOME">> ${USER_HOME}/.bashrc
         echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ${USER_HOME}/.bashrc
     fi

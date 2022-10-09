95d954d07bb67b29d932b17812d087371a119ff5
Chen Haifeng <haifeng.chen@intel.com>
2022-10-09 10:20:55 +0800

    Support job waiter for completion for exec on all nodes from head (#879)

--- python/cloudtik/core/_private/cluster/cluster_utils.py
+++ python/cloudtik/core/_private/cluster/cluster_utils.py
@@ -56,24 +56,40 @@ def run_on_cluster(
         run_env: str = "auto",
         with_output: bool = False,
         _allow_uninitialized_state: bool = False) -> str:
-    # We default this to True to maintain backwards-compatibility
-    # In the future we would want to support disabling login-shells
-    # and interactivity.
-    call_context.set_allow_interactive(True)
-
     use_internal_ip = config.get("bootstrapped", False)
-
-    provider = _get_node_provider(config["provider"], config["cluster_name"])
     head_node = get_running_head_node(
         config,
         _allow_uninitialized_state=_allow_uninitialized_state)
-    updater = create_node_updater_for_exec(
+
+    return run_on_node(
         config=config,
         call_context=call_context,
         node_id=head_node,
+        cmd=cmd,
+        run_env=run_env,
+        with_output=with_output,
+        is_head_node=True,
+        use_internal_ip=use_internal_ip
+    )
+
+
+def run_on_node(
+        config: Dict[str, Any],
+        call_context: CallContext,
+        node_id: str,
+        cmd: str = None,
+        run_env: str = "auto",
+        with_output: bool = False,
+        is_head_node: bool = False,
+        use_internal_ip: bool = True) -> str:
+    provider = _get_node_provider(config["provider"], config["cluster_name"])
+    updater = create_node_updater_for_exec(
+        config=config,
+        call_context=call_context,
+        node_id=node_id,
         provider=provider,
         start_commands=[],
-        is_head_node=True,
+        is_head_node=is_head_node,
         use_internal_ip=use_internal_ip)
 
     exec_out = updater.cmd_executor.run(

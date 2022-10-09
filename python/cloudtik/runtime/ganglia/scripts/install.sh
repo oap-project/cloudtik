0af072f32d414b8d6508ec8a9294d8d2e0257c75
Chen Haifeng <haifeng.chen@intel.com>
2022-10-09 09:18:40 +0800

    Architecture aware handling for ganglia modpython (#878)

--- python/cloudtik/runtime/ganglia/scripts/install.sh
+++ python/cloudtik/runtime/ganglia/scripts/install.sh
@@ -28,7 +28,8 @@ function install_ganglia_monitor_python() {
     GANGLIA_LIB=/usr/lib/ganglia
     GANGLIA_MODULE_PYTHON=${GANGLIA_LIB}/modpython.so
     if [ ! -f "${GANGLIA_MODULE_PYTHON}" ]; then
-        wget -q -P /tmp https://d30257nes7d4fq.cloudfront.net/downloads/ganglia/modpython.so
+        arch=$(uname -m)
+        wget -q https://d30257nes7d4fq.cloudfront.net/downloads/ganglia/modpython-${arch}.so -O /tmp/modpython.so
         sudo cp /tmp/modpython.so ${GANGLIA_LIB} && sudo chmod 644 ${GANGLIA_MODULE_PYTHON}
     fi
     sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y ganglia-monitor-python python2.7-dev > /dev/null

73bd7cb12f392ff55d9b75bde35859a6e2cfc7bd
Chen Haifeng <haifeng.chen@intel.com>
2022-10-25 10:26:30 +0800

    Add cluster workflow examples with shell scripts or python API (#914)

---
+++ example/workflows/example-on-demmand-job.py
@@ -0,0 +1,25 @@
+from cloudtik.core.api import Cluster
+
+# The cluster config file
+cluster_config = "./your-cluster-config.yaml"
+
+# The cmd to run Spark Pi job
+job_cmd = "spark-submit --master yarn --deploy-mode cluster --name spark-pi --class org.apache.spark.examples.SparkPi --conf spark.yarn.submit.waitAppCompletion=false $SPARK_HOME/examples/jars/spark-examples_2.12-3.2.1.jar 100"
+
+# Create a new cluster client object to operate
+cluster = Cluster(cluster_config)
+
+# Start a cluster
+cluster.start()
+
+# Wait for all workers ready
+cluster.wait_for_ready()
+
+# Submit a job and wait for the spark job to finish
+cluster.exec(job_cmd, job_waiter="spark")
+
+# Stop the cluster
+cluster.stop()
+
+# The above can also be done with a single exec call
+# cluster.exec(job_cmd, start=True, wait_for_workers=True, job_waiter="spark", stop=True)

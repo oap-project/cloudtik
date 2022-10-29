09bb82c945cf2015b8901e473442c3d037ef885a
Chen Haifeng <haifeng.chen@intel.com>
2022-10-29 13:05:14 +0800

    Add azure identity depedency as azure cli don't depend azure identity now (#936)

--- python/setup.py
+++ python/setup.py
@@ -68,7 +68,8 @@ setup_spec.extras = {
         "botocore",
     ],
     "azure": [
-        "azure-cli==2.41.0",
+        "azure-cli==2.40.0",
+        "azure-identity==1.11.0"
         "azure-storage-blob==12.11.0",
         "azure-storage-file-datalake==12.6.0",
     ],

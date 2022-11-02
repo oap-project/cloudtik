a4bfee14d213daca9eacafbab844fc79502eae14
Chen Haifeng <haifeng.chen@intel.com>
2022-11-02 14:07:19 +0800

    Add workflow examples for ml cases (#951)

---
+++ example/workflows/example-on-demmand-ml-job.py
@@ -0,0 +1,25 @@
+from cloudtik.core.api import Cluster
+
+# The cluster config file: change to your file
+cluster_config = "./your-cluster-config.yaml"
+
+# The python job: change to your job file
+job_file = "./cloudtik/example/ml/jobs/spark-mlflow-hyperopt-horovod-tensorflow.py"
+
+# Create a new cluster client object to operate
+cluster = Cluster(cluster_config)
+
+# Start a cluster
+cluster.start()
+
+# Wait for all workers ready
+cluster.wait_for_ready()
+
+# Submit a job and wait for the spark job to finish
+cluster.submit(job_file, tmux=True, job_waiter="tmux")
+
+# Stop the cluster
+cluster.stop()
+
+# The above can also be done with a single submit call
+# cluster.submit(job_file, start=True, wait_for_workers=True, tmux=True, job_waiter="tmux", stop=True)
--- example/workflows/example-on-demmand-spark-job.py
+++ example/workflows/example-on-demmand-spark-job.py
@@ -1,9 +1,9 @@
 from cloudtik.core.api import Cluster
 
-# The cluster config file
+# The cluster config file: change to your file
 cluster_config = "./your-cluster-config.yaml"
 
-# The cmd to run Spark Pi job
+# The cmd to run: change to your cmd
 job_cmd = "spark-submit --master yarn --deploy-mode cluster --name spark-pi --class org.apache.spark.examples.SparkPi --conf spark.yarn.submit.waitAppCompletion=false $SPARK_HOME/examples/jars/spark-examples_2.12-3.2.1.jar 100"
 
 # Create a new cluster client object to operate
@@ -15,7 +15,7 @@ cluster.start()
 # Wait for all workers ready
 cluster.wait_for_ready()
 
-# Submit a job and wait for the spark job to finish
+# Exec a job and wait for the spark job to finish
 cluster.exec(job_cmd, job_waiter="spark")
 
 # Stop the cluster

c4e57883015182a97892a308d56f233dd9eb8b28
Chen Haifeng <haifeng.chen@intel.com>
2023-01-11 16:30:37 +0800

    Tensorflow examples for MNIST and synthetic data (#1052)

---
+++ example/ml/jobs/tensorflow/mnist-tensorflow-keras-horovod-run.py
@@ -0,0 +1,175 @@
+# Copyright 2019 Uber Technologies, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import argparse
+import os
+
+# Benchmark settings
+parser = argparse.ArgumentParser(description='TensorFlow Keras MNIST Example',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--num-proc', type=int,
+                    help='number of worker processes for training')
+parser.add_argument('--batch-size', type=int, default=128,
+                    help='input batch size')
+parser.add_argument('--base-lr', type=float, default=0.001,
+                    help='learning rate for a single GPU')
+parser.add_argument('--epochs', type=int, default=2,
+                    help='number of epochs to train (default: 2)')
+
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+
+parser.add_argument('--gloo', action='store_true', dest='use_gloo',
+                    help='Run Horovod using the Gloo controller. This will '
+                         'be the default if Horovod was not built with MPI support.')
+parser.add_argument('--mpi', action='store_true', dest='use_mpi',
+                    help='Run Horovod using the MPI controller. This will '
+                         'be the default if Horovod was built with MPI support.')
+
+
+import tensorflow as tf
+import horovod.tensorflow.keras as hvd
+
+
+def train_horovod(learning_rate):
+    args.cuda = not args.no_cuda
+
+    # Horovod: initialize Horovod.
+    hvd.init()
+
+    # Horovod: pin GPU to be used to process local rank (one GPU per process)
+    if args.cuda:
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        if gpus:
+            tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+    (mnist_images, mnist_labels), _ = \
+        tf.keras.datasets.mnist.load_data(path='mnist-%d.npz' % hvd.rank())
+
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32),
+                 tf.cast(mnist_labels, tf.int64))
+    )
+    dataset = dataset.repeat().shuffle(10000).batch(args.batch_size)
+
+    mnist_model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
+        tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    # Horovod: adjust learning rate based on number of GPUs.
+    scaled_lr = learning_rate * hvd.size()
+    opt = tf.optimizers.Adam(scaled_lr)
+
+    # Horovod: add Horovod DistributedOptimizer.
+    opt = hvd.DistributedOptimizer(
+        opt, backward_passes_per_step=1, average_aggregated_gradients=True)
+
+    # Horovod: Specify `experimental_run_tf_function=False` to ensure TensorFlow
+    # uses hvd.DistributedOptimizer() to compute gradients.
+    mnist_model.compile(loss=tf.losses.SparseCategoricalCrossentropy(),
+                        optimizer=opt,
+                        metrics=['accuracy'],
+                        experimental_run_tf_function=False)
+
+    callbacks = [
+        # Horovod: broadcast initial variable states from rank 0 to all other processes.
+        # This is necessary to ensure consistent initialization of all workers when
+        # training is started with random weights or restored from a checkpoint.
+        hvd.callbacks.BroadcastGlobalVariablesCallback(0),
+
+        # Horovod: average metrics among workers at the end of every epoch.
+        #
+        # Note: This callback must be in the list before the ReduceLROnPlateau,
+        # TensorBoard or other metrics-based callbacks.
+        hvd.callbacks.MetricAverageCallback(),
+
+        # Horovod: using `lr = 1.0 * hvd.size()` from the very beginning leads to worse final
+        # accuracy. Scale the learning rate `lr = 1.0` ---> `lr = 1.0 * hvd.size()` during
+        # the first three epochs. See https://arxiv.org/abs/1706.02677 for details.
+        hvd.callbacks.LearningRateWarmupCallback(initial_lr=scaled_lr, warmup_epochs=3, verbose=1),
+    ]
+
+    # Horovod: save checkpoints only on worker 0 to prevent other workers from corrupting them.
+    if hvd.rank() == 0:
+        callbacks.append(tf.keras.callbacks.ModelCheckpoint('./checkpoint-{epoch}.h5'))
+
+    # Horovod: write logs on worker 0.
+    verbose = 1 if hvd.rank() == 0 else 0
+
+    # Train the model.
+    # Horovod: adjust number of steps based on number of GPUs.
+    mnist_model.fit(dataset,
+                    steps_per_epoch=500 // hvd.size(),
+                    callbacks=callbacks,
+                    epochs=args.epochs,
+                    verbose=verbose)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    # CloudTik cluster preparation or information
+    from cloudtik.runtime.spark.api import ThisSparkCluster
+
+    cluster = ThisSparkCluster()
+
+    # Scale the cluster as need
+    # cluster.scale(workers=1)
+
+    # Wait for all cluster workers to be ready
+    cluster.wait_for_ready(min_workers=1)
+
+    # Total worker cores
+    cluster_info = cluster.get_info()
+
+    if not args.num_proc:
+        total_workers = cluster_info.get("total-workers")
+        if total_workers:
+            args.num_proc = total_workers
+        if not args.num_proc:
+            args.num_proc = 1
+
+    worker_ips = cluster.get_worker_node_ips()
+
+    #  Hyperopt training function
+    import horovod
+
+    # Set the parameters
+    num_proc = args.num_proc
+    print("Train processes: {}".format(num_proc))
+
+    # Generate the host list
+    host_slots = ["{}:1".format(worker_ip) for worker_ip in worker_ips]
+    hosts = ",".join(host_slots)
+    print("Hosts to run:", hosts)
+
+    learning_rate = args.base_lr
+    print("Train learning rate: {}".format(learning_rate))
+
+    horovod.run(
+        train_horovod, args=(learning_rate,),
+        num_proc=num_proc, hosts=hosts,
+        use_gloo=args.use_gloo, use_mpi=args.use_mpi,
+        verbose=2)

f6b6b62f465d585741428c5e899ae2cbd10dfbfc
Qing Yao <qing.yao@intel.com>
2023-01-13 09:54:19 +0800

    Add bert prepare data codes.

--- tools/benchmarks/ml/mlperf/scripts/bootstrap-mlperf.sh
+++ tools/benchmarks/ml/mlperf/scripts/bootstrap-mlperf.sh
@@ -3,6 +3,11 @@
 args=$(getopt -a -o h::p: -l head:: -- "$@")
 eval set -- "${args}"
 
+USER_HOME=/home/$(whoami)
+BENCHMARK_TOOL_HOME=/cloudtik/fs/benchmark-tools
+MLPERF_HOME=$BENCHMARK_TOOL_HOME/mlperf
+GITSPACE=$USER_HOME/gitspace
+
 while true
 do
     case "$1" in
@@ -32,9 +37,7 @@ function is_head_node() {
 function prepare() {
     source ~/.bashrc
     sudo apt-get update -y
-    export USER_HOME=/home/$(whoami)
-    BENCHMARK_TOOL_HOME=$USER_HOME/runtime/benchmark-tools
-    MLPERF_HOME=$BENCHMARK_TOOL_HOME/mlperf
+
     mkdir -p $BENCHMARK_TOOL_HOME
     sudo chown $(whoami) $BENCHMARK_TOOL_HOME
 }
@@ -50,6 +53,16 @@ function install_libaries() {
 }
 
 function install_mlperf() {
+    pip install "git+https://github.com/mlcommons/logging.git@2.0.0"
+}
+
+function install_mlperf_scripts() {
+  mkdir -p $GITSPACE
+  cd $GITSPACE
+  git clone https://github.com/oap-project/cloudtik.git
+  rm -rf $MLPERF_HOME/*
+  cp -r cloudtik/tools/benchmarks/ml/mlperf/* $MLPERF_HOME/
+
 
 }
 
@@ -61,4 +74,5 @@ prepare
 install_tools
 install_libaries
 install_mlperf
+install_mlperf_scripts
 configure_mlperf

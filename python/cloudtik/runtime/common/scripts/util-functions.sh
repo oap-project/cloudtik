fbb4df14af5bff3e37b0155cc72df09c0c0dec1f
Chen Haifeng <haifeng.chen@intel.com>
2023-01-15 14:38:16 +0800

    Add set_node_ip_address util function

--- python/cloudtik/runtime/common/scripts/util-functions.sh
+++ python/cloudtik/runtime/common/scripts/util-functions.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 function set_head_address() {
-    if [ $IS_HEAD_NODE == "true" ]; then
-        if [ ! -n "${CLOUDTIK_NODE_IP}" ]; then
-            HEAD_ADDRESS=$(hostname -I | awk '{print $1}')
+    if [ -z "${HEAD_ADDRESS}" ]; then
+        if [ $IS_HEAD_NODE == "true" ]; then
+            if [ ! -n "${CLOUDTIK_NODE_IP}" ]; then
+                HEAD_ADDRESS=$(hostname -I | awk '{print $1}')
+            else
+                HEAD_ADDRESS=${CLOUDTIK_NODE_IP}
+            fi
         else
-            HEAD_ADDRESS=${CLOUDTIK_NODE_IP}
+            if [ ! -n "${CLOUDTIK_HEAD_IP}" ]; then
+                echo "Error: CLOUDTIK_HEAD_IP environment variable should be set."
+                exit 1
+            else
+                HEAD_ADDRESS=${CLOUDTIK_HEAD_IP}
+            fi
         fi
-    else
-        if [ ! -n "${CLOUDTIK_HEAD_IP}" ]; then
-            echo "Error: CLOUDTIK_HEAD_IP environment variable should be set."
-            exit 1
+    fi
+}
+
+function set_node_ip_address() {
+    if [ -z "${NODE_IP_ADDRESS}" ]; then
+        if [ ! -n "${CLOUDTIK_NODE_IP}" ]; then
+            NODE_IP_ADDRESS=$(hostname -I | awk '{print $1}')
         else
-            HEAD_ADDRESS=${CLOUDTIK_HEAD_IP}
+            NODE_IP_ADDRESS=${CLOUDTIK_NODE_IP}
         fi
     fi
 }

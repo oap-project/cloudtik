48a1cbeee6b9e387012841a917323b07e2fb3d07
Chen Haifeng <haifeng.chen@intel.com>
2023-02-01 18:01:12 +0800

    Make compile spark and benchmark scripts consistent

--- runtime/spark/scripts/compile-spark.sh
+++ runtime/spark/scripts/compile-spark.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 CLOUDTIK_HOME=$(cd $(dirname ${BASH_SOURCE[0]})/../../..;pwd)
-SPARK_VERSION=$1
+SPARK_VERSION=3.3.0
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+    --version)
+        # Override for the hadoop version.
+        shift
+        SPARK_VERSION=$1
+        ;;
+    *)
+        echo "Usage: compile-spark.sh [ --version ]"
+        exit 1
+    esac
+    shift
+done
 
 rm -rf /tmp/spark
 git clone https://github.com/apache/spark.git /tmp/spark
 cd /tmp/spark && git checkout v${SPARK_VERSION}
 
-for patch in $CLOUDTIK_HOME/runtime/spark/spark-${SPARK_VERSION}/optimizations/*.patch; do
-    git apply $patch
-done
+if [ -d "$CLOUDTIK_HOME/runtime/spark/spark-${SPARK_VERSION}" ]; then
+    for patch in $CLOUDTIK_HOME/runtime/spark/spark-${SPARK_VERSION}/optimizations/*.patch; do
+        git apply $patch
+    done
+fi
 
 ./dev/make-distribution.sh --name hadoop3 --tgz -Phadoop-3 -Dhadoop.version=3.3.1 -Phive -Phive-thriftserver -Pyarn \
  -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true

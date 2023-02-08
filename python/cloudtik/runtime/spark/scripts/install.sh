7d5fe5a17bf1647d2d11dba7937456bcf4f987ff
Qing Yao <qing.yao@intel.com>
2023-02-08 16:34:28 +0800

    Remove unnecessary codes about fuse_dfs arch.

--- python/cloudtik/runtime/spark/scripts/install.sh
+++ python/cloudtik/runtime/spark/scripts/install.sh
@@ -157,13 +157,8 @@ function install_spark_with_cloud_jars() {
 
 function install_hdfs_fuse() {
     arch=$(uname -m)
-    if [ "${arch}" == "aarch64" ]; then
-        arch_hadoop="aarch64"
-    else
-        arch_hadoop="x64"
-    fi
-    sudo wget  -q --show-progress https://d30257nes7d4fq.cloudfront.net/downloads/hadoop/fuse_dfs-${HADOOP_VERSION}-${arch_hadoop} -O /usr/bin/fuse_dfs
-    sudo wget  -q --show-progress https://d30257nes7d4fq.cloudfront.net/downloads/hadoop/fuse_dfs_wrapper-${HADOOP_VERSION}-${arch_hadoop}.sh -O /usr/bin/fuse_dfs_wrapper.sh
+    sudo wget  -q --show-progress https://d30257nes7d4fq.cloudfront.net/downloads/hadoop/fuse_dfs-${HADOOP_VERSION}-${arch} -O /usr/bin/fuse_dfs
+    sudo wget  -q --show-progress https://d30257nes7d4fq.cloudfront.net/downloads/hadoop/fuse_dfs_wrapper-${HADOOP_VERSION}.sh -O /usr/bin/fuse_dfs_wrapper.sh
     sudo chmod +x /usr/bin/fuse_dfs
     sudo chmod +x /usr/bin/fuse_dfs_wrapper.sh
 }

5a248730da9315c0b53810c704a229d5a6b3df45
haojinIntel <hao.jin@intel.com>
2023-03-16 09:24:28 +0800

    Simplify the installation for jemalloc

--- tools/benchmarks/ml/intelai-models/common/scripts/setenv.sh
+++ tools/benchmarks/ml/intelai-models/common/scripts/setenv.sh
@@ -12,8 +12,8 @@ fi
 export INTELAI_MODELS_WORKSPACE=$INTELAI_MODELS_WORKSPACE
 
 # Set Jemalloc Preload for better performance
-export LD_PRELOAD=/home/cloudtik/runtime/benchmark-tools/jemalloc_lib/lib/libjemalloc.so:$LD_PRELOAD
+export LD_PRELOAD=$HOME/anaconda3/envs/cloudtik/lib/libjemalloc.so:$LD_PRELOAD
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
 
 # Set IOMP preload for better performance
-export LD_PRELOAD=/home/cloudtik/anaconda3/envs/cloudtik/lib/libiomp5.so:$LD_PRELOAD
+export LD_PRELOAD=$HOME/anaconda3/envs/cloudtik/lib/libiomp5.so:$LD_PRELOAD

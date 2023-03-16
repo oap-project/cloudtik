6f9972c7e3a5cce75d32c8326580a2f822b41ff8
haojinIntel <hao.jin@intel.com>
2023-03-16 14:22:08 +0800

    Remove useless code

--- tools/benchmarks/ml/intelai-models/resnext-32x16d/scripts/prepare-data.sh
+++ tools/benchmarks/ml/intelai-models/resnext-32x16d/scripts/prepare-data.sh
@@ -13,7 +13,6 @@ function download_data() {
     mkdir -p $RESNEXT101_DATA
     cd $RESNEXT101_DATA
     wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
-    #wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_test_v10102019.tar
 }
 
 function prepare_val_data() {

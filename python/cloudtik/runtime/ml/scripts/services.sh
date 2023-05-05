7738bf8ef49da4fc5b59bb432f7911a218eec30b
Chen Haifeng <haifeng.chen@intel.com>
2023-05-05 11:23:12 +0800

    ML: MLflow use cloud database if it is configured

--- python/cloudtik/runtime/ml/scripts/services.sh
+++ python/cloudtik/runtime/ml/scripts/services.sh
@@ -6,8 +6,14 @@ RUNTIME_PATH=$USER_HOME/runtime
 case "$1" in
 start-head)
     # Start MLflow service
+    if [ "${CLOUD_DATABASE}" == "true" ] && [ "$ML_WITH_CLOUD_DATABASE" != "false" ]; then
+        BACKEND_STORE_URI=mysql://${CLOUD_DATABASE_USERNAME}:${CLOUD_DATABASE_PASSWORD}@${CLOUD_DATABASE_HOSTNAME}:${CLOUD_DATABASE_PORT}/mlflow
+    else
+        BACKEND_STORE_URI=sqlite:///${MLFLOW_DATA}/mlflow.db
+    fi
+
     MLFLOW_DATA=$RUNTIME_PATH/mlflow
-    nohup mlflow server --backend-store-uri sqlite:///${MLFLOW_DATA}/mlflow.db --default-artifact-root ${MLFLOW_DATA}/mlruns --host 0.0.0.0 -p 5001 >${MLFLOW_DATA}/logs/mlflow.log 2>&1 &
+    nohup mlflow server --backend-store-uri ${BACKEND_STORE_URI} --default-artifact-root ${MLFLOW_DATA}/mlruns --host 0.0.0.0 -p 5001 >${MLFLOW_DATA}/logs/mlflow.log 2>&1 &
     ;;
 stop-head)
     # Stop MLflow service

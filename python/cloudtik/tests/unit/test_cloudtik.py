a6beee8501168e41a2a89e30f7e3cd86f8f3f582
Chen Haifeng <haifeng.chen@intel.com>
2023-05-30 21:01:51 +0800

    Core: scale up only consider the head resources (#1473)

--- python/cloudtik/tests/unit/test_cloudtik.py
+++ python/cloudtik/tests/unit/test_cloudtik.py
@@ -886,10 +886,17 @@ class CloudTikTest(unittest.TestCase):
             self.fail("Test config did not pass validation test!")
 
         config["blah"] = "blah"
-        with pytest.raises(ValidationError):
+        try:
             validate_config(config)
+        except Exception:
+            self.fail("Config should allow additional properties!")
         del config["blah"]
 
+        del config["cluster_name"]
+        with pytest.raises(ValidationError):
+            validate_config(config)
+
+        config = copy.deepcopy(MOCK_DEFAULT_CONFIG)
         del config["provider"]
         with pytest.raises(ValidationError):
             validate_config(config)

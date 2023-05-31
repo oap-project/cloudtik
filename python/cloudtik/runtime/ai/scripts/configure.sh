1a049301490f07d4265bd481cd1c2280b276967d
Chen Haifeng <haifeng.chen@intel.com>
2023-05-31 15:28:17 +0800

    AI: to use common scripts infrastructure

--- python/cloudtik/runtime/ai/scripts/configure.sh
+++ python/cloudtik/runtime/ai/scripts/configure.sh
@@ -4,7 +4,7 @@
 BIN_DIR=`dirname "$0"`
 ROOT_DIR="$(dirname "$(dirname "$BIN_DIR")")"
 
-args=$(getopt -a -o h:: -l head::,node_ip_address:,head_address: -- "$@")
+args=$(getopt -a -o h:: -l head:: -- "$@")
 eval set -- "${args}"
 
 IS_HEAD_NODE=false
@@ -15,14 +15,6 @@ do
     case "$1" in
     -h|--head)
         IS_HEAD_NODE=true
-        ;;
-    --node_ip_address)
-        NODE_IP_ADDRESS=$2
-        shift
-        ;;
-    --head_address)
-        HEAD_ADDRESS=$2
-        shift
         ;;
     --)
         shift
@@ -36,28 +28,15 @@ done
 # API cloud credential configuration functions
 . "$ROOT_DIR"/common/scripts/api-credential.sh
 
+# Util functions
+. "$ROOT_DIR"/common/scripts/util-functions.sh
+
 function prepare_base_conf() {
     output_dir=/tmp/ai/conf
     rm -rf  $output_dir
     mkdir -p $output_dir
     source_dir=$(cd $(dirname ${BASH_SOURCE[0]})/..;pwd)/conf
     cp -r $source_dir/* $output_dir
-}
-
-function set_head_address() {
-    if [ $IS_HEAD_NODE == "true" ]; then
-        if [ ! -n "${NODE_IP_ADDRESS}" ]; then
-            HEAD_ADDRESS=$(hostname -I | awk '{print $1}')
-        else
-            HEAD_ADDRESS=${NODE_IP_ADDRESS}
-        fi
-    else
-        if [ ! -n "${HEAD_ADDRESS}" ]; then
-            # Error: no head address passed
-            echo "Error: head ip address should be passed."
-            exit 1
-        fi
-    fi
 }
 
 function configure_system_folders() {

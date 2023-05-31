47b5ef5ece103e26eda2a014947b2a2d191205a7
Chen Haifeng <haifeng.chen@intel.com>
2023-05-31 14:45:19 +0800

    Runtime: Ganglia to use common scripts infrastructure

--- python/cloudtik/runtime/ganglia/scripts/services.sh
+++ python/cloudtik/runtime/ganglia/scripts/services.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
 
-case "$1" in
-start-head)
-    sudo service apache2 start
-    sudo service gmetad start
-    sudo service ganglia-monitor start
+command=$1
+shift
+
+# Parsing arguments
+IS_HEAD_NODE=false
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+    -h|--head)
+        IS_HEAD_NODE=true
+        ;;
+    *)
+        echo "Unknown argument passed."
+        exit 1
+    esac
+    shift
+done
+
+case "$command" in
+start)
+    if [ $IS_HEAD_NODE == "true" ]; then
+        sudo service apache2 start
+        sudo service gmetad start
+        sudo service ganglia-monitor start
+    else
+        sudo service ganglia-monitor start
+    fi
     ;;
-stop-head)
-    sudo service ganglia-monitor stop
-    sudo service gmetad stop
-    sudo service apache2 stop
-    ;;
-start-worker)
-    sudo service ganglia-monitor start
-    ;;
-stop-worker)
-    sudo service ganglia-monitor stop
+stop)
+    if [ $IS_HEAD_NODE == "true" ]; then
+        sudo service ganglia-monitor stop
+        sudo service gmetad stop
+        sudo service apache2 stop
+    else
+        sudo service ganglia-monitor stop
+    fi
     ;;
 -h|--help)
-    echo "Usage: $0 start-head|stop-head|start-worker|stop-worker" >&2
+    echo "Usage: $0 start|stop --head" >&2
     ;;
 *)
-    echo "Usage: $0 start-head|stop-head|start-worker|stop-worker" >&2
+    echo "Usage: $0 start|stop --head" >&2
     ;;
 esac
 

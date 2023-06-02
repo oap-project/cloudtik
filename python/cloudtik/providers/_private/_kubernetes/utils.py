6b41e94515ec60726bf56c68dc1d4c3bfd6ab1b9
Chen Haifeng <haifeng.chen@intel.com>
2023-06-02 09:19:44 +0800

    Provider: auto generate public key from private key if not specified

--- python/cloudtik/providers/_private/_kubernetes/utils.py
+++ python/cloudtik/providers/_private/_kubernetes/utils.py
@@ -220,6 +220,11 @@ def cleanup_orphan_pvcs(cluster_name, namespace):
 
 
 def get_key_pair_path_for_kubernetes(config):
-    key_pair_file_path = "~/.ssh/cloudtik_kubernetes_{}_{}.pem".format(config["provider"]["cloud_provider"]["type"],
-                                                                  config["cluster_name"])
+    provider = config["provider"]
+    if "cloud_provider" in provider:
+        key_pair_file_path = "~/.ssh/cloudtik_kubernetes_{}_{}.pem".format(
+            provider["cloud_provider"]["type"], config["cluster_name"])
+    else:
+        key_pair_file_path = "~/.ssh/cloudtik_kubernetes_{}.pem".format(
+            config["cluster_name"])
     return key_pair_file_path

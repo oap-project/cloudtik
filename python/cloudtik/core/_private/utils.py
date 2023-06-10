daa54fdb99b1e00580bd2314f8dbdcbe4be64a54
Chen Haifeng <haifeng.chen@intel.com>
2023-06-10 19:59:41 +0800

    Core: fix the verbose option append to cmds issue (#1532)

--- python/cloudtik/core/_private/utils.py
+++ python/cloudtik/core/_private/utils.py
@@ -3174,6 +3174,6 @@ def parse_resources(resources_str: str, ) -> Dict[str, int]:
 def with_verbose_option(cmds, call_context):
     _cli_logger = call_context.cli_logger
     if _cli_logger.verbosity_overriden:
-        cmds += "--verbose={}".format(_cli_logger.verbosity)
+        cmds += ["--verbose={}".format(_cli_logger.verbosity)]
         return True
     return False

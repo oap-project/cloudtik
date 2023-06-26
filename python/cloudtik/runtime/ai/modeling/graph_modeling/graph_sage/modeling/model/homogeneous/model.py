4e21ab66092b832c1d12865b04363ebb09fca59c
Chen Haifeng <haifeng.chen@intel.com>
2023-06-26 12:19:37 +0800

    AI: GraphSAGE shared inference process among inductive and transductive (#1595)

--- python/cloudtik/runtime/ai/modeling/graph_modeling/graph_sage/modeling/model/homogeneous/model.py
+++ python/cloudtik/runtime/ai/modeling/graph_modeling/graph_sage/modeling/model/homogeneous/model.py
@@ -14,10 +14,16 @@ limitations under the License.
 Author: Chen Haifeng
 """
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from dgl.nn import SAGEConv
+
+import tqdm
+from dgl.dataloading import (
+    DataLoader,
+    MultiLayerFullNeighborSampler,
+)
 
 
 class GraphSAGE(nn.Module):
@@ -89,3 +95,122 @@ class GraphSAGEModel(nn.Module):
 
     def get_inference_inputs(self, g):
         raise NotImplementedError("The final model must implement this method.")
+
+    def get_encoder_inputs(self, input_nodes, blocks):
+        return self.get_inputs(input_nodes, blocks)
+
+    def inference(self, g, x, device, batch_size):
+        """Layer-wise inference algorithm to compute node embeddings.
+        for the nodes of the entire graph.
+        Inference with the GraphSAGE model on full neighbors (i.e. without
+        neighbor sampling).
+
+        g : the entire graph.
+        x : the input of entire node set (feature or None if using node id)
+        """
+        # During inference with sampling, multi-layer blocks are very
+        # inefficient because lots of computations in the first few layers
+        # are repeated. Therefore, we compute the representation of all nodes
+        # layer by layer.  The nodes on each layer are of course splitted in
+        # batches.
+        sampler = MultiLayerFullNeighborSampler(1)
+        dataloader = DataLoader(
+            g,
+            torch.arange(g.num_nodes()).to(g.device),
+            sampler,
+            device=device,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=4,
+        )
+        buffer_device = torch.device("cpu")
+        pin_memory = buffer_device != device
+        # compute representations layer by layer
+        for l, layer in enumerate(self.encoder.layers):
+            y = torch.empty(
+                g.num_nodes(),
+                self.encoder.hidden_size if l != len(
+                    self.encoder.layers) - 1 else self.encoder.out_feats,
+                device=buffer_device,
+                pin_memory=pin_memory,
+            )
+            x = x.to(device) if x is not None else x
+            # within a layer iterate over nodes in batches
+            with dataloader.enable_cpu_affinity():
+                for input_nodes, output_nodes, blocks in tqdm.tqdm(
+                    dataloader, desc="Inference"
+                ):
+                    if x is not None:
+                        h = x[input_nodes]
+                    else:
+                        # The first layer will come to here if x is None
+                        h = self.get_encoder_inputs(input_nodes, blocks)
+                    h = h.to(device)
+                    h = layer(blocks[0], h)
+                    if l != len(self.encoder.layers) - 1:
+                        h = F.relu(h)
+                    y[output_nodes] = h.to(buffer_device)
+                x = y
+        return y
+
+    def inference_nodes(self, g, x, nids, device, batch_size):
+        """Layer-wise inference algorithm to compute GNN node embeddings.
+        Inference with the GraphSAGE model on full neighbors (i.e. without
+        neighbor sampling).
+
+        g : the entire graph.
+        x : the input of entire node set (feature or None if using node id)
+        nids : Tensor of the indices of the node ids to be computed
+        """
+        # for batch, we don't compute representations layer by layer
+        # instead we do it with batch and then layers
+        num_nodes = nids.size(dim=0)
+        if batch_size == 0:
+            batch_size = num_nodes
+
+        sampler = MultiLayerFullNeighborSampler(
+            num_layers=self.encoder.layers)
+        dataloader = DataLoader(
+            g,
+            nids.to(g.device),
+            sampler,
+            device=device,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=4,
+        )
+        buffer_device = torch.device("cpu")
+        pin_memory = buffer_device != device
+
+        y = torch.empty(
+            num_nodes,
+            self.encoder.out_feats,
+            device=buffer_device,
+            pin_memory=pin_memory,
+        )
+        y_start = 0
+
+        x = x.to(device) if x is not None else x
+        # first iterating over the mini batches
+        with dataloader.enable_cpu_affinity():
+            for input_nodes, output_nodes, blocks in tqdm.tqdm(
+                    dataloader, desc="Inference"
+            ):
+                if x is not None:
+                    h = x[input_nodes]
+                else:
+                    h = self.get_encoder_inputs(input_nodes, blocks)
+                h = h.to(device)
+                for l, layer in enumerate(self.encoder.layers):
+                    h = layer(blocks[l], h)
+                    if l != len(self.encoder.layers) - 1:
+                        h = F.relu(h)
+
+                # store the final output
+                num_output = output_nodes.size(dim=0)
+                y[y_start:y_start+num_output] = h.to(buffer_device)
+                y_start += num_output
+
+        return y

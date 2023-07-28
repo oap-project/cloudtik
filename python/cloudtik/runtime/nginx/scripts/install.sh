a000e9f17c30829398c663e4e96216ed995df5b5
Chen Haifeng <haifeng.chen@intel.com>
2023-07-28 15:43:55 +0800

    Runtime: fix the output dir path for HAProxy configuring

--- python/cloudtik/runtime/nginx/scripts/install.sh
+++ python/cloudtik/runtime/nginx/scripts/install.sh
@@ -14,7 +14,7 @@ function install_nginx() {
     if ! command -v nginx &> /dev/null
     then
         deb_arch=$(get_deb_arch)
-        wget -O - -q http://nginx.org/keys/nginx_signing.key | sudo apt-key add - > /dev/null
+        wget -O - -q http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
         echo "deb [arch=${deb_arch}] http://nginx.org/packages/mainline/ubuntu/ $(lsb_release -cs) nginx" | sudo tee /etc/apt/sources.list.d/nginx.list > /dev/null
         sudo apt-get -qq update -y > /dev/null
         sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq nginx -y > /dev/null

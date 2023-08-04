aac26427b045dd78e944e29f5b7bfe855732526c
Chen Haifeng <haifeng.chen@intel.com>
2023-08-04 21:10:07 +0800

    Runtime: support prometheus federation from multiple servers

--- python/cloudtik/runtime/prometheus/utils.py
+++ python/cloudtik/runtime/prometheus/utils.py
@@ -22,8 +22,11 @@ PROMETHEUS_SERVICE_PORT_CONFIG_KEY = "port"
 PROMETHEUS_HIGH_AVAILABILITY_CONFIG_KEY = "high_availability"
 PROMETHEUS_SERVICE_DISCOVERY_CONFIG_KEY = "service_discovery"
 PROMETHEUS_SCRAPE_SCOPE_CONFIG_KEY = "scrape_scope"
+PROMETHEUS_SCRAPE_SERVICES_CONFIG_KEY = "scrape_services"
 PROMETHEUS_SCRAPE_TAGS_CONFIG_KEY = "scrape_tags"
 PROMETHEUS_SCRAPE_LABELS_CONFIG_KEY = "scrape_labels"
+PROMETHEUS_SCRAPE_EXCLUDE_LABELS_CONFIG_KEY = "scrape_exclude_labels"
+
 
 PROMETHEUS_SERVICE_NAME = "prometheus"
 PROMETHEUS_SERVICE_PORT_DEFAULT = 9090
@@ -33,6 +36,7 @@ PROMETHEUS_SERVICE_DISCOVERY_CONSUL = "CONSUL"
 
 PROMETHEUS_SCRAPE_SCOPE_LOCAL = "local"
 PROMETHEUS_SCRAPE_SCOPE_WORKSPACE = "workspace"
+PROMETHEUS_SCRAPE_SCOPE_FEDERATION = "federation"
 
 
 def _get_config(runtime_config: Dict[str, Any]):
@@ -66,8 +70,8 @@ def _get_runtime_logs():
 def _with_runtime_environment_variables(
         runtime_config, config):
     runtime_envs = {}
-
     prometheus_config = _get_config(runtime_config)
+    cluster_runtime_config = config.get(RUNTIME_CONFIG_KEY)
 
     service_port = _get_service_port(prometheus_config)
     runtime_envs["PROMETHEUS_SERVICE_PORT"] = service_port
@@ -79,7 +83,6 @@ def _with_runtime_environment_variables(
     sd = prometheus_config.get(PROMETHEUS_SERVICE_DISCOVERY_CONFIG_KEY)
     if not sd:
         # auto decide
-        cluster_runtime_config = config.get(RUNTIME_CONFIG_KEY)
         if get_service_discovery_runtime(cluster_runtime_config):
             sd = PROMETHEUS_SERVICE_DISCOVERY_CONSUL
         else:
@@ -102,12 +105,13 @@ def _with_runtime_environment_variables(
     scrape_scope = prometheus_config.get(PROMETHEUS_SCRAPE_SCOPE_CONFIG_KEY)
     if scrape_scope == PROMETHEUS_SCRAPE_SCOPE_WORKSPACE:
         # make sure
-        cluster_runtime_config = config.get(RUNTIME_CONFIG_KEY)
         if not get_service_discovery_runtime(cluster_runtime_config):
             raise RuntimeError("Service discovery service is needed for workspace scoped scrape.")
-
         sd = PROMETHEUS_SERVICE_DISCOVERY_CONSUL
-        scrape_scope = PROMETHEUS_SCRAPE_SCOPE_WORKSPACE
+    if scrape_scope == PROMETHEUS_SCRAPE_SCOPE_FEDERATION:
+        if not get_service_discovery_runtime(cluster_runtime_config):
+            raise RuntimeError("Service discovery service is needed for federation scoped scrape.")
+        sd = PROMETHEUS_SERVICE_DISCOVERY_CONSUL
     elif not scrape_scope:
         scrape_scope = PROMETHEUS_SCRAPE_SCOPE_LOCAL
 
@@ -175,9 +179,12 @@ def _get_runtime_services(
 
 def _get_config_file(scrape_scope):
     home_dir = _get_home_dir()
-    config_file_name = "scrape-config-workspace-consul.yaml" if (
-            scrape_scope == PROMETHEUS_SCRAPE_SCOPE_WORKSPACE) else (
-        "scrape-config-local-consul.yaml")
+    if scrape_scope == PROMETHEUS_SCRAPE_SCOPE_WORKSPACE:
+        config_file_name = "scrape-config-workspace-consul.yaml"
+    elif scrape_scope == PROMETHEUS_SCRAPE_SCOPE_FEDERATION:
+        config_file_name = "scrape-config-federation-consul.yaml"
+    else:
+        config_file_name = "scrape-config-local-consul.yaml"
     return os.path.join(home_dir, "conf", config_file_name)
 
 
@@ -188,18 +195,27 @@ def configure_scrape(head):
     sd = os.environ.get("PROMETHEUS_SERVICE_DISCOVERY")
     if sd == PROMETHEUS_SERVICE_DISCOVERY_CONSUL:
         # tags and labels only support service discovery based scrape (consul)
+        services = prometheus_config.get(PROMETHEUS_SCRAPE_SERVICES_CONFIG_KEY)
         tags = prometheus_config.get(PROMETHEUS_SCRAPE_TAGS_CONFIG_KEY)
         labels = prometheus_config.get(PROMETHEUS_SCRAPE_LABELS_CONFIG_KEY)
-        if tags or labels:
+        exclude_labels = prometheus_config.get(PROMETHEUS_SCRAPE_EXCLUDE_LABELS_CONFIG_KEY)
+
+        if tags or labels or exclude_labels or services:
             scrape_scope = os.environ.get("PROMETHEUS_SCRAPE_SCOPE")
             config_file = _get_config_file(scrape_scope)
-            update_scrape_config(config_file, tags, labels)
+            update_scrape_config(
+                config_file, services, tags, labels, exclude_labels)
 
 
-def update_scrape_config(config_file, tags, labels):
+def update_scrape_config(config_file, services, tags, labels, exclude_labels):
     def update_contents(config_object):
         scrape_configs = config_object["scrape_configs"]
         for scrape_config in scrape_configs:
+            if services:
+                sd_configs = scrape_config["consul_sd_configs"]
+                for sd_config in sd_configs:
+                    # replace the services if specified
+                    sd_config["services"] = services
             if tags:
                 sd_configs = scrape_config["consul_sd_configs"]
                 for sd_config in sd_configs:
@@ -209,10 +225,20 @@ def update_scrape_config(config_file, tags, labels):
                 relabel_configs = get_list_for_update(scrape_config, "relabel_configs")
                 for label_key, label_value in labels.items():
                     relabel_config = {
-                        "action": "keep",
                         "source_labels": ["__meta_consul_service_metadata_{}".format(
                             label_key)],
-                        "regex": label_value
+                        "regex": label_value,
+                        "action": "keep",
+                    }
+                    relabel_configs.append(relabel_config)
+            if exclude_labels:
+                relabel_configs = get_list_for_update(scrape_config, "relabel_configs")
+                for label_key, label_value in exclude_labels.items():
+                    relabel_config = {
+                        "source_labels": ["__meta_consul_service_metadata_{}".format(
+                            label_key)],
+                        "regex": label_value,
+                        "action": "drop",
                     }
                     relabel_configs.append(relabel_config)
 

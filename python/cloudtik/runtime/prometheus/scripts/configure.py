098127f4a93d885cdbd74600c51273503d72df82
Chen Haifeng <haifeng.chen@intel.com>
2023-08-04 17:41:22 +0800

    Runtime: Prometheus scrape to support user labels and tags (#1739)

---
+++ python/cloudtik/runtime/prometheus/scripts/configure.py
@@ -0,0 +1,20 @@
+import argparse
+import os
+
+from cloudtik.runtime.prometheus.utils import configure_scrape
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Configuring runtime.")
+    parser.add_argument('--head', action='store_true', default=False,
+                        help='Configuring for head node.')
+    args = parser.parse_args()
+
+    high_availability = os.environ.get("PROMETHEUS_HIGH_AVAILABILITY")
+    if high_availability == "true" or args.head:
+        configure_scrape()
+
+
+if __name__ == "__main__":
+    main()

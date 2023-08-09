d575b1811cc0e9a584ad26c2f68434b5de91ebae
Chen Haifeng <haifeng.chen@intel.com>
2023-08-09 10:25:53 +0800

    refine

--- python/cloudtik/runtime/haproxy/discover_backend_servers.py
+++ python/cloudtik/runtime/haproxy/discover_backend_servers.py
@@ -5,7 +5,7 @@ from cloudtik.core._private.util.pull.pull_job import PullJob
 from cloudtik.runtime.common.service_discovery.consul \
     import query_services, query_service_nodes, get_service_address
 from cloudtik.runtime.haproxy.admin_api import list_backend_servers, enable_backend_slot, disable_backend_slot, \
-    add_backend_slot
+    add_backend_slot, get_backend_server_address
 from cloudtik.runtime.haproxy.utils import update_configuration, get_backend_server_name
 
 logger = logging.getLogger(__name__)
@@ -23,7 +23,7 @@ def _update_backend(backend_name, backend_servers):
 
         missing_backend_servers = []
         for backend_server in backend_servers:
-            server_address = "%s:%d" % (backend_server[0], backend_server[1])
+            server_address = get_backend_server_address(backend_server)
             if server_address in active_servers:
                 # Ignore backends already set
                 del active_servers[server_address]
